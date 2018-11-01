181ee85868c5f911a3afd757999144a674d92e9d
Shinmera <shinmera@tymoon.eu>
2018-11-01 10:17:38 +0100

    Camera shake

--- main.lisp
+++ main.lisp
@@ -32,44 +32,6 @@
 (define-asset (leaf square) mesh
     (make-rectangle 8 8 :align :topleft))
 
-(define-subject camera (trial:2d-camera)
-  ((zoom :initarg :zoom :initform 2 :accessor zoom)
-   (target-size :initarg :target-size :accessor target-size)
-   (target :initarg :target :initform NIL :accessor target)
-   (surface :initform NIL :accessor surface))
-  (:default-initargs
-   :location (vec 0 0)
-   :target-size (vec (* 8 60) 0)))
-
-(defmethod enter :after ((camera camera) (scene scene))
-  (setf (target camera) (unit :player scene))
-  (setf (surface camera) (unit :surface scene)))
-
-(define-handler (camera trial:tick) (ev)
-  (let ((loc (location camera))
-        (surface (surface camera))
-        (size (target-size camera)))
-    (when (target camera)
-      (let ((tar (location (target camera))))
-        (vsetf loc (vx tar) (vy tar))
-        (nv- loc (v/ size 2))))
-    (setf (vx loc) (max 0 (min (- (* (first (size surface)) (tile-size surface))
-                                  (vx size))
-                               (vx loc))))
-    (setf (vy loc) (max 0 (min (- (* (second (size surface)) (tile-size surface))
-                                  (vy size))
-                               (vy loc))))))
-
-(define-handler (camera resize) (ev)
-  (setf (zoom camera) (float (/ (width ev) (vx (target-size camera)))))
-  (setf (vy (target-size camera)) (/ (height ev) (zoom camera))))
-
-(defmethod project-view ((camera camera) ev)
-  (let ((z (zoom camera)))
-    (reset-matrix *view-matrix*)
-    (scale-by z z z *view-matrix*)
-    (translate (nv- (vxy_ (location camera))) *view-matrix*)))
-
 (defclass main (trial:main)
   ((scene :initform NIL))
   (:default-initargs :clear-color (vec 61/255 202/255 245/255)
