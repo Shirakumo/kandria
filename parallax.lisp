3a969d14e5c13f3f5b2cbc476c6947025cc93c27
Shinmera <shinmera@tymoon.eu>
2018-11-06 21:45:24 +0100

    Undo camera centering since it's just a hassle

--- parallax.lisp
+++ parallax.lisp
@@ -11,8 +11,7 @@
     (with-pushed-attribs
       (setf (uniform program "aspect") (float (/ (width *context*)
                                                  (height *context*))))
-      (setf (uniform program "offset") (nv- (vapply (location (unit :camera T)) floor)
-                                            (target-size (unit :camera T))))
+      (setf (uniform program "offset") (vapply (location (unit :camera T)) floor))
       (setf (uniform program "parallax") 0)
       (gl:active-texture :texture0)
       (gl:bind-texture :texture-2d (gl-name (texture parallax)))
