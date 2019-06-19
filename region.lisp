53c52436dfdae214aa8e019c25a0da97617c1b9c
Shinmera <shinmera@tymoon.eu>
2019-06-19 21:02:03 +0200

    Add region object as layered top-level container

--- region.lisp
+++ region.lisp
@@ -1,22 +1,107 @@
 (in-package #:org.shirakumo.fraf.leaf)
 
+(defclass region (container-unit)
+  ((objects :initform NIL)
+   (author :initform "Anonymous" :initarg :author :accessor author)
+   (version :initform "0.0.0" :initarg :version :accessor version)
+   (description :initform "" :initarg :description :accessor description)
+   (preview :initform NIL :initarg :preview :accessor preview)))
+
+(defmethod initialize-instance :after ((region region) &key (layers 5))
+  (let ((objects (make-array layers)))
+    (dotimes (i layers)
+      (setf (aref objects i) (make-array 0 :adjustable T :fill-pointer T)))
+    (setf (objects region) objects)))
+
+(defgeneric layer-index (unit))
+
+(defmethod layer-index ((unit unit)) 0)
+
+(defmethod enter ((unit unit) (region region))
+  (let ((layer (+ (layer-index unit)
+                  (floor (length (objects region)) 2))))
+    (vector-push-extend unit (aref (objects region) layer))))
+
+(defmethod enter ((chunk chunk) (region region))
+  (dotimes (layer (length (objects region)))
+    (vector-push-extend chunk (aref (objects region) layer))))
+
+(defmethod leave ((unit unit) (region region))
+  (let ((layer (+ (layer-index unit)
+                  (floor (length (objects region)) 2))))
+    (array-utils:vector-pop-position*
+     (aref (objects region) layer)
+     (position unit (aref (objects region) layer)))))
+
+(defmethod leave ((chunk chunk) (region region))
+  (dotimes (layer (length (objects region)))
+    (array-utils:vector-pop-position*
+     (aref (objects region) layer)
+     (position chunk (aref (objects region) layer)))))
+
+(defclass region-iterator (for:iterator)
+  ((layer :initarg :layer :accessor layer)
+   (start :initform 0 :accessor start)))
+
+(defmethod for:has-more ((iterator region-iterator))
+  (< (layer iterator) (length (for:object iterator))))
+
+(defmethod for:next ((iterator region-iterator))
+  (let ((layer (aref (for:object iterator) (layer iterator))))
+    (prog1 (aref layer (start iterator))
+      (incf (start iterator))
+      (when (<= (length layer) (start iterator))
+        (setf (start iterator) 0)
+        (loop for i from (1+ (layer iterator)) below (length (for:object iterator))
+              while (= 0 (length (aref (for:object iterator) i)))
+              finally (setf (layer iterator) i))))))
+
+(defmethod (setf for:current) ((unit unit) (iterator region-iterator))
+  (setf (aref (aref (for:object iterator) (layer iterator))
+              (start iterator))
+        unit))
+
+(defmethod for:make-iterator ((region region) &key)
+  (make-instance 'region-iterator
+                 :object (objects region)
+                 :layer (or (position 0 (objects region) :key #'length :test-not #'=)
+                            MOST-POSITIVE-FIXNUM)))
+
 (defclass version () ())
-(defclass v0 (version) ())
+
+(defun coerce-version (symbol)
+  (flet ((bail () (error "No such version ~s." symbol)))
+    (let* ((class (or (find-class symbol NIL) (bail))))
+      (unless (subtypep class 'version) (bail))
+      (make-instance class))))
 
 (defgeneric load-region (source scene))
-(defgeneric save-region (scene target &key version &allow-other-keys))
+(defgeneric save-region (region target &key version &allow-other-keys))
 (defgeneric decode-region-payload (source target version))
 (defgeneric encode-region-payload (source target version))
 
-(defmethod save-region (scene (pathname pathname) &key version (if-exists :supersede))
-  (zip:with-output-to-zipfile (file pathname :if-exists if-exists)
-    (save-region scene file :version version)))
+(defmethod save-region :around (region target &rest args &key (version T))
+  (cond ((eql T version)
+         ;; KLUDGE: latest version should be determined automatically.
+         (apply #'call-next-method region target :version (make-instance 'v0) args))
+        ((typep version 'version)
+         (call-next-method))
+        (T
+         (error "VERSION must be an instance of VERSION or T."))))
 
-(defmethod save-region (scene (file zip::zipwriter) &key version)
-  ;; FIXME: metadata
-  (let ((meta (make-sexp-stream `(:identifier region :version ,(type-of version))))
+(defmethod save-region (region (pathname pathname) &key version (if-exists :supersede))
+  (zip:with-output-to-zipfile (file pathname :if-exists if-exists)
+    (save-region region file :version version)))
+
+(defmethod save-region (region (file zip::zipwriter) &key version)
+  ;; FIXME: preview
+  (let ((meta (make-sexp-stream (list :identifier 'region
+                                      :version (type-of version))
+                                (list :author (author region)
+                                      :version (version region)
+                                      :description (description region))))
         (data (string-binary-stream (with-output-to-string (stream)
-                                      (encode-region-payload scene stream version)))))
+                                      (encode-region-payload region stream version)))))
     (zip:write-zipentry file "meta.lisp" meta :file-write-date (get-universal-time))
     (zip:write-zipentry file "data" data :file-write-date (get-universal-time))))
 
@@ -31,19 +116,14 @@
       (error "Malformed region file."))
     (destructuring-bind (header information)
         (parse-sexp-vector (zip:zipfile-entry-contents meta))
-      (declare (ignore information))
-      (decode-region-payload
-       (zip:zipfile-entry-contents data)
-       scene
-       (destructuring-bind (&key identifier version) header
-         (assert (eql 'region identifier))
-         (coerce-version version))))))
-
-(defun coerce-version (symbol)
-  (flet ((bail () (error "No such version ~s." symbol)))
-    (let* ((class (or (find-class symbol NIL) (bail))))
-      (unless (subtypep class 'version) (bail))
-      (make-instance class))))
+      (let ((region (apply #'make-instance 'region information)))
+        (enter region scene)
+        (decode-region-payload
+         (zip:zipfile-entry-contents data)
+         region
+         (destructuring-bind (&key identifier version) header
+           (assert (eql 'region identifier))
+           (coerce-version version)))))))
 
 (defun parse-sexp-vector (vector)
   (loop with eof = (make-symbol "EOF")
@@ -98,49 +178,3 @@
            ,@body)))))
 
 (trivial-indent:define-indentation define-decoder (4 4 &body))
-
-(define-decoder (scene v0) (buffer)
-  (destructuring-bind (&key name entities) (first (parse-sexp-vector buffer))
-    (setf (slot-value scene 'name) name)
-    (loop for (type . initargs) in entities
-          do (enter (decode type initargs) scene))
-    scene))
-
-(define-encoder (scene v0) (stream)
-  (with-leaf-io-syntax
-    (write (list :name (name scene)
-                 :entities (for:for ((entity over scene)
-                                     (_ collecting (encode entity)))))
-           :stream stream
-           :case :downcase)))
-
-(define-decoder (player v0) (initargs)
-  (destructuring-bind (x y) (getf initargs :location)
-    (make-instance 'player :location (vec2 x y))))
-
-(define-encoder (player v0) (_)
-  `(player :location ,(encode (location player))))
-
-(define-decoder (chunk v0) (initargs)
-  (destructuring-bind (&key name location size background tileset layers) initargs
-    (make-instance 'chunk :name name
-                          :location (vec2 (first location) (second location))
-                          :size (cons (first size) (second size))
-                          :tileset (asset (first tileset) (second tileset))
-                          :background (asset (first tileset) (second tileset)))))
-
-(define-encoder (chunk v0) (_)
-  `(chunk :name ,(name chunk)
-          :location ,(encode (location chunk))
-          :size (,(car (size chunk)) ,(cdr (size chunk)))
-          :tileset ,(encode (tileset chunk))
-          :background ,(encode (background chunk))
-          :layers ()))
-
-(define-encoder (vec2 v0) (_)
-  (list (vx vec2)
-        (vy vec2)))
-
-(define-encoder (asset v0) (_)
-  (list (name (pool asset))
-        (name asset)))
