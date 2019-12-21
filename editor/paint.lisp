06538b1881662b55b1e2ca8d4a421c760f474bd1
Shinmera <shinmera@tymoon.eu>
2019-12-21 20:25:02 +0100

    minor

--- editor/paint.lisp
+++ editor/paint.lisp
@@ -17,6 +17,15 @@
     (:placing
      (paint-tile tool event))))
 
+(defmethod handle ((event key-press) (tool paint))
+  (case (key event)
+    (:1 (setf (layer (sidebar (editor tool))) -2))
+    (:2 (setf (layer (sidebar (editor tool))) -1))
+    (:3 (setf (layer (sidebar (editor tool)))  0))
+    (:4 (setf (layer (sidebar (editor tool))) +1))
+    (:5 (setf (layer (sidebar (editor tool))) +2))
+    (:0 (setf (layer (sidebar (editor tool))) +3))))
+
 (defun paint-tile (tool event)
   (let* ((loc (nvalign (nv- (mouse-world-pos (pos event)) (/ +tile-size+ 2)) +tile-size+))
          (loc (vec (vx loc) (vy loc) (layer (sidebar (editor tool)))))
