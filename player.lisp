095f6df3cf137c389e7d04a3440e9d5a43b78593
Shinmera <shinmera@tymoon.eu>
2020-03-12 14:35:22 +0100

    Some stuff for the animation editor.

--- player.lisp
+++ player.lisp
@@ -174,15 +174,8 @@ void main(){
            (vsetf acc (vx proj) (vy proj)))))
       (:attacking
        (nv* acc 0)
-       (when (if (eq 'heavy-ground (trial::sprite-animation-name (animation player)))
-                 (= 12 (frame-idx player))
-                 (= 6 (frame-idx player)))
-         (let ((other (scan (surface player) (hurtbox player))))
-           (when (typep other 'enemy)
-             (let ((dir (nvunit (v- (location other) (location player)))))
-               (if (eq 'heavy-ground (trial::sprite-animation-name (animation player)))
-                   (vsetf (acceleration other) (* (vx dir) 5) 2)
-                   (vsetf (acceleration other) (* (vx dir) 3) 1))))))
+       (let ((frame (frame-data player)))
+         (nv+ acc (frame-velocity frame)))
        (when (eql 'stand (trial::sprite-animation-name (animation player)))
          (setf (state player) :normal)))
       (:dying
