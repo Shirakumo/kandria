102032311437ddef9d8813fe48de41c2334cedc4
Shinmera <shinmera@tymoon.eu>
2020-04-18 15:03:47 +0200

    Fix save format and restore behaviour.

--- versions/save-v0.lisp
+++ versions/save-v0.lisp
@@ -29,36 +29,37 @@
                              :element-type 'character)
     (let ((create-new (list NIL))
           (ephemeral (list NIL)))
-      (labels ((payload (named entity target)
-                 (handler-case
-                     (setf (cdr target) (list* (cons (name named) (encode entity)) (cdr target)))
-                   (no-applicable-encoder (e)
-                     (declare (ignore e)))))
-               (recurse (parent)
-                 (for:for ((entity over parent))
-                   (cond ((typep entity 'ephemeral)
-                          (payload entity entity ephemeral)
-                          (when (typep entity 'container)
-                            (recurse entity)))
-                         (T
-                          (payload parent entity create-new))))))
-        (recurse region))
+      (macrolet ((add (target value)
+                   `(handler-case
+                        (setf (cdr ,target) (list* ,value (cdr ,target)))
+                      (no-applicable-encoder (e)
+                        (declare (ignore e))))))
+        (labels ((recurse (parent)
+                   (for:for ((entity over parent))
+                     (cond ((typep entity 'ephemeral)
+                            (add ephemeral (list* (name entity) (encode entity)))
+                            (when (typep entity 'container)
+                              (recurse entity)))
+                           (T
+                            (add create-new (list* (name parent) (type-of entity) (encode entity))))))))
+          (recurse region)))
       (princ* (list :create-new (rest create-new)
                     :ephemeral (rest ephemeral))
               stream))))
 
-(define-encoder (player save-v0) (_b _p)
-  (rest (call-next-method)))
-
 (define-encoder (animatable save-v0) (_b _p)
-  `(,(type-of animatable)
-    :location ,(encode (location animatable))
+  `(:location ,(encode (location animatable))
     :direction ,(direction animatable)
     :state ,(state animatable)
     :animation ,(sprite-animation-name (animation animatable))
     :frame ,(frame animatable)
     :health ,(health animatable)
     :stun-time ,(stun-time animatable)))
+
+(define-encoder (moving-platform save-v0) (_b _p)
+  `(:location ,(encode (location moving-platform))
+    :velocity ,(encode (velocity moving-platform))
+    :state ,(state moving-platform)))
 
 (define-decoder (world save-v0) (_b packet)
   (destructuring-bind (&key region)
@@ -91,20 +92,21 @@
     (setf (quest:status quest:task) status)))
 
 (define-decoder (region save-v0) (initargs packet)
-  (destructuring-bind (&key create-new ephemeral)
+  (destructuring-bind (&key create-new ephemeral (delete-existing T))
       (first (parse-sexps (packet-entry (format NIL "regions/~(~a~).lisp" (name region))
                                         packet :element-type 'character)))
     ;; Remove all entities that are not ephemeral
-    (labels ((recurse (parent)
-               (for:for ((entity over parent))
-                 (typecase entity
-                   ((not ephemeral) (leave entity parent))
-                   (container (recurse entity))))))
-      (recurse region))
+    (when delete-existing
+      (labels ((recurse (parent)
+                 (for:for ((entity over parent))
+                   (typecase entity
+                     ((not ephemeral) (leave entity parent))
+                     (container (recurse entity))))))
+        (recurse region)))
     ;; Add new entities that exist in the state
-    (loop for (name . state) in create-new
+    (loop for (name type . state) in create-new
           for parent = (unit name (scene-graph region))
-          do (enter (decode-payload (cdr state) (make-instance (car state)) packet save-v0) parent))
+          do (enter (decode-payload state (make-instance type) packet save-v0) parent))
     ;; Update state on ephemeral ones
     (loop for (name . state) in ephemeral
           for unit = (unit name (scene-graph region))
@@ -126,3 +128,10 @@
     (setf (health animatable) health)
     (setf (stun-time animatable) stun-time)
     animatable))
+
+(define-decoder (moving-platform save-v0) (initargs _p)
+  (destructuring-bind (&key location velocity state) initargs
+    (setf (location moving-platform) (decode 'vec2 location))
+    (setf (velocity moving-platform) (decode 'vec2 velocity))
+    (setf (state moving-platform) state)
+    moving-platform))
