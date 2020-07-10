15870808210f9e88d6873e77295aca903225f20f
Shinmera <shinmera@tymoon.eu>
2020-07-10 12:06:14 +0200

    Animation fixes

--- regions/test/data.lisp
+++ regions/test/data.lisp
@@ -1,3 +1,3 @@
-(player :location (-155.67575 -176.0) :name player)
-(wolf :location (-473.08035 -184.0) :name :wolf)
+(player :location (-324.80243 -176.0) :name player)
+(wolf :location (-221.76941 -168.0) :name :wolf)
 (chunk :name start :location (0.0 0.0) :size (62.0 34.0) :tile-data (leaf debug) :pixel-data "data/START.raw" :layers ("data/START-0.raw" "data/START-1.raw" "data/START-2.raw" "data/START-3.raw" "data/START-4.raw"))
