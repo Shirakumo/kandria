16fcead8d81437c8c1d5df6ea34a80e0d6d4f3fd
Shinmera <shinmera@tymoon.eu>
2020-07-10 14:17:44 +0200

    Minor

--- enemy.lisp
+++ enemy.lisp
@@ -1,11 +1,12 @@
 (in-package #:org.shirakumo.fraf.leaf)
 
 (define-shader-entity enemy (animatable)
-  ((bsize :initform (vec 12.0 8.0))
+  ((bsize :initform (vec 8.0 8.0))
    (cooldown :initform 0.0 :accessor cooldown))
   (:default-initargs
    :sprite-data (asset 'leaf 'wolf)))
 
+(defmethod capable-p ((enemy enemy) (edge crawl-edge)) T)
 (defmethod capable-p ((enemy enemy) (edge jump-edge)) T)
 
 (defmethod handle :before ((ev tick) (enemy enemy))
@@ -19,8 +20,7 @@
     (nv+ vel (v* +vgrav+ dt))
     (cond ((svref collisions 2)
            (when (<= -0.1 (vx vel) 0.1)
-             (setf (vx vel) 0))
-           (setf (vy vel) (max 0 (vy vel))))
+             (setf (vx vel) 0)))
           (T
            (when (<= 2 (vx vel))
              (setf (vx vel) (* (vx vel) (damp* 0.90 dt))))))
@@ -50,10 +50,10 @@
               (setf (animation enemy) 'jump))
              ((null (svref collisions 2))
               (setf (animation enemy) 'fall))
-             ((< 0 (abs (vx vel)) 1.0)
-              (setf (animation enemy) 'walk))
-             ((<= 1.0 (abs (vx vel)))
+             ((<= 0.75 (abs (vx vel)))
               (setf (animation enemy) 'run))
+             ((< 0 (abs (vx vel)))
+              (setf (animation enemy) 'walk))
              (T
               (setf (animation enemy) 'stand)))))))
 
@@ -62,7 +62,8 @@
 
 (defmethod movement-speed ((enemy wolf))
   (case (state enemy)
-    (:normal 0.9)
+    (:crawling 0.4)
+    (:normal 0.5)
     (T 2.0)))
 
 (defmethod handle-ai-states ((enemy wolf) ev)
@@ -73,36 +74,42 @@
          (col (collisions enemy))
          (vel (velocity enemy)))
     (ecase (state enemy)
-      (:normal
-       (cond ((< distance 400)
-              (setf (state enemy) :approach))
-             ((null (path enemy))
-              (ignore-errors (move-to (vec (+ (vx (location enemy)) (- (random 100) 50)) (vy (location enemy))) enemy)))))
-      (:approach
-       ;; FIXME: This should be reached even when there is a path being executed right now.
-       (cond ((< distance 200)
-              (setf (path enemy) ())
-              (setf (state enemy) :attack))
-             ((null (path enemy))
-              (ignore-errors (move-to (location player) enemy)))))
-      (:evade
-       (if (< 100 distance)
-           (setf (state enemy) :attack)
-           (let ((dir (signum (- (vx eloc) (vx ploc)))))
-             (when (and (svref col 2) (svref col (if (< 0 dir) 1 3)))
-               (setf (vy vel) 3.2))
-             (setf (vx vel) (* dir 2.0)))))
-      (:attack
-       (cond ((< 500 distance)
-              (setf (state enemy) :normal))
-             ((< distance 80)
-              (setf (state enemy) :evade))
-             (T
-              (setf (direction enemy) (signum (- (vx (location player)) (vx (location enemy)))))
-              (cond ((svref col (if (< 0 (direction enemy)) 1 3))
-                     (setf (vy vel) 2.0)
-                     (setf (vx vel) (* (direction enemy) 2.0)))
-                    ((svref col 2)
-                     (setf (vy vel) 0.0)
-                     ;; Check that tackle would even be possible to hit (no obstacles)
-                     (start-animation 'tackle enemy)))))))))
+      ((:normal :crawling)
+       (cond ;; ((< distance 400)
+             ;;  (setf (state enemy) :approach))
+         ((and (null (path enemy)) (<= (cooldown enemy) 0))
+          (if (ignore-errors (move-to (vec (+ (vx (location enemy)) (- (random 200) 50)) (+ (vy (location enemy)) 64)) enemy))
+              (setf (cooldown enemy) (+ 0.5 (expt (random 1.5) 2)))
+              (setf (cooldown enemy) 0.1)))
+         ((null (path enemy))
+          (decf (cooldown enemy) (dt ev)))))
+      (:approach (setf (state enemy) :normal))
+      ;; (:approach
+      ;;  ;; FIXME: This should be reached even when there is a path being executed right now.
+      ;;  (cond ((< distance 200)
+      ;;         (setf (path enemy) ())
+      ;;         (setf (state enemy) :attack))
+      ;;        ((null (path enemy))
+      ;;         (ignore-errors (move-to (location player) enemy)))))
+      ;; (:evade
+      ;;  (if (< 100 distance)
+      ;;      (setf (state enemy) :attack)
+      ;;      (let ((dir (signum (- (vx eloc) (vx ploc)))))
+      ;;        (when (and (svref col 2) (svref col (if (< 0 dir) 1 3)))
+      ;;          (setf (vy vel) 3.2))
+      ;;        (setf (vx vel) (* dir 2.0)))))
+      ;; (:attack
+      ;;  (cond ((< 500 distance)
+      ;;         (setf (state enemy) :normal))
+      ;;        ((< distance 80)
+      ;;         (setf (state enemy) :evade))
+      ;;        (T
+      ;;         (setf (direction enemy) (signum (- (vx (location player)) (vx (location enemy)))))
+      ;;         (cond ((svref col (if (< 0 (direction enemy)) 1 3))
+      ;;                (setf (vy vel) 2.0)
+      ;;                (setf (vx vel) (* (direction enemy) 2.0)))
+      ;;               ((svref col 2)
+      ;;                (setf (vy vel) 0.0)
+      ;;                ;; Check that tackle would even be possible to hit (no obstacles)
+      ;;                (start-animation 'tackle enemy))))))
+      )))
