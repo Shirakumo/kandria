b13494e297c6f0d62ae78c085da255b2de6aff4f
Shinmera <shinmera@tymoon.eu>
2020-07-28 11:48:10 +0200

    Start work on steam build integration.

--- releases/kandria-release.asd
+++ releases/kandria-release.asd
@@ -1,4 +1,4 @@
 (asdf:defsystem kandria-release
   :components ((:file "release"))
-  :depends-on (:pathname-utils :zippy :deploy)
+  :depends-on (:alexandria :cl-ppcre :pathname-utils :zippy :deploy)
   :perform (asdf:build-op (op c) (uiop:symbol-call :release :release)))
--- releases/release.lisp
+++ releases/release.lisp
@@ -12,6 +12,19 @@
 (defun version ()
   (asdf:component-version (asdf:find-system "leaf")))
 
+(defun output ()
+  (pathname-utils:to-directory #.(or *compile-file-pathname* *load-pathname*)))
+
+(defun query-pass ()
+  (format *query-io* "~&Enter Steam password:~%> ")
+  (read-line *query-io*))
+
+(defun file-replace (in out replacements)
+  (let ((content (alexandria:read-file-into-string in)))
+    (loop for (search replace) in replacements
+          do (setf content (cl-ppcre:regex-replace search content replace)))
+    (alexandria:write-string-into-file content out :if-exists :supersede)))
+
 (defun build ()
   (with-simple-restart (continue "Treat build as successful")
     (sb-ext:run-program "sbcl" (list "--eval" "(asdf:make :leaf :force T)"
@@ -19,12 +32,10 @@
                         :search T :output T :input NIL)))
 
 (defun deploy ()
-  (let* ((source (asdf:system-source-directory "leaf"))
-         (vername (format NIL "leaf-~a" (version)))
-         (bindir (pathname-utils:subdirectory source "bin"))
-         (reldir (pathname-utils:subdirectory source "releases"))
-         (verdir (pathname-utils:subdirectory reldir vername)))
-    (ensure-directories-exist reldir)
+  (let* ((vername (format NIL "leaf-~a" (version)))
+         (bindir (pathname-utils:subdirectory (asdf:system-source-directory "leaf") "bin"))
+         (verdir (pathname-utils:subdirectory (output) vername)))
+    (ensure-directories-exist verdir)
     (deploy:copy-directory-tree bindir verdir :copy-root NIL)
     (uiop:delete-file-if-exists (merge-pathnames "trial.log" verdir))
     verdir))
@@ -45,23 +56,32 @@
       (push "windows" systems))
     systems))
 
-(defun itch (release)
+(defun itch (release &key &allow-other-keys)
   (sb-ext:run-program "butler" (list "push" (uiop:native-namestring release)
                                      (format NIL "Shinmera/kandria:~{~a~^-~}" (release-systems release))
                                      "--userversion" (version))
                       :search T :output T))
 
-(defun humble (release))
+(defun steam (release &key (branch "developer") (preview 1) &allow-other-keys)
+  (let ((template (make-pathname :name "app-build" :type "vdf" :defaults (output)))
+        (build (make-pathname :name "app-build" :type "vdf" :defaults release)))
+    (file-replace template build `(("$CONTENT" ,(uiop:native-namestring release))
+                                   ("$BRANCH" ,(or branch ""))
+                                   ("$PREVIEW" ,(if preview "1" "0"))))
+    (sb-ext:run-program "steamcmd.sh" (list "+login" "shinmera" (query-pass) "+run_app_build" (uiop:native-namestring build) "+quit")
+                        :search T :output T)))
 
-(defun steam (release))
+(defun upload (release &rest args &key (services T))
+  (case services ((T :itch) (apply #'itch release args)))
+  (case services ((T :steam) (apply #'steam release args))))
 
-(defun release (&key push)
+(defun release (&key upload)
   (deploy:status 0 "Releasing ~a" (version))
   (deploy:status 1 "Deploying to release directory")
   (let ((release (deploy)))
     (deploy:status 1 "Creating bundle zip")
     (bundle release)
-    (when push
-      (deploy:status 1 "Uploading to itch.io")
-      (itch release))
+    (when upload
+      (deploy:status 1 "Uploading")
+      (upload release :services upload))
     release))
