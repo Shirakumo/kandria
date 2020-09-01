dd710eb65bfb6b2730d3de3aa0e1b5d844c7b738
Shinmera <shinmera@tymoon.eu>
2020-09-01 16:05:12 +0200

    Fix some stuff, include compiler for mess to html.

---
+++ doc/compile.lisp
@@ -0,0 +1,35 @@
+#|
+sbcl --noinform --load "$0" --eval '(generate-all)' --quit && exit
+|#
+
+(ql:quickload '(cl-markless-plump lass) :silent T)
+
+(defun style ()
+  (lass:compile-and-write
+   '(article
+     :max-width 800px
+     :margin 0 auto
+     (h1
+      :text-align center
+      :font-size 2em)
+     (img
+      :max-width 100%)
+     (blockquote
+      :border-left 0.2em solid gray
+      :margin-left 1em
+      :padding-left 1em))))
+
+(defun generate (file)
+  (handler-bind ((file-exists (lambda (e)
+                                (declare (ignore e))
+                                (invoke-restart 'supersede))))
+    (cl-markless:output (cl-markless:parse file T)
+                        :target (make-pathname :type "html" :defaults file)
+                        :format (make-instance 'org.shirakumo.markless.plump:plump
+                                               :css (style)))))
+
+(defun generate-all ()
+  (dolist (file (directory (make-pathname :name :wild :type "mess"
+                                          :defaults #.(or *compile-file-pathname* *load-pathname* *default-pathname-defaults*))))
+    (with-simple-restart (continue "Ignore ~a" file)
+      (generate file))))
