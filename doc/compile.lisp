1848406a8a3bf03454464cab7e19cee5165c6ebc
Shinmera <shinmera@tymoon.eu>
2020-10-12 13:40:00 +0200

    Add youtube embed option

--- doc/compile.lisp
+++ doc/compile.lisp
@@ -2,7 +2,44 @@
 sbcl --noinform --load "$0" --eval '(generate-all)' --quit && exit
 |#
 
-(ql:quickload '(cl-markless-plump lass lquery) :silent T)
+(ql:quickload '(cl-markless-plump lass lquery cl-ppcre) :silent T)
+
+(defun youtube-code (url)
+  (let ((pieces (nth-value 1 (cl-ppcre:scan-to-strings "((http|https)://)?(www\\.)?(youtube\\.com|youtu\\.be)/(watch\\?v=)?([0-9a-zA-Z_\\-]{4,12})" url))))
+    (when pieces (aref pieces 5))))
+
+(defclass youtube (cl-markless-components:video)
+  ())
+
+(defmethod cl-markless:output-component ((component youtube) (target plump-dom:nesting-node) (format cl-markless-plump:plump))
+  (let ((element (plump-dom:make-element target "iframe")))
+    (setf (plump-dom:attribute element "width") "100%")
+    (setf (plump-dom:attribute element "height") "240")
+    (setf (plump-dom:attribute element "frameborder") "no")
+    (setf (plump-dom:attribute element "allowfullscreen") "yes")
+    (setf (plump-dom:attribute element "src")
+          (format NIL "//www.youtube.com/embed/~a?" (youtube-code (cl-markless-components:target component))))
+    (loop for option in (cl-markless-components:options component)
+          do (typecase option
+               (cl-markless-components:autoplay-option
+                (setf (plump-dom:attribute element "src")
+                      (format NIL "~aautoplay=1&" (plump-dom:attribute element "src"))))
+               (cl-markless-components:loop-option
+                (setf (plump-dom:attribute element "src")
+                      (format NIL "~aloop=1&" (plump-dom:attribute element "src"))))
+               (cl-markless-components:width-option
+                (setf (plump-dom:attribute element "width")
+                      (format NIL "~d~(~a~)"
+                              (cl-markless-components:size option)
+                              (cl-markless-components:unit option))))
+               (cl-markless-components:height-option
+                (setf (plump-dom:attribute element "height")
+                      (format NIL "~d~(~a~)"
+                              (cl-markless-components:size option)
+                              (cl-markless-components:unit option))))
+               (cl-markless-components:float-option
+                (setf (plump-dom:attribute element "style")
+                      (format NIL "float:~(~a~)" (cl-markless-components:direction option))))))))
 
 (defun style ()
   (lass:compile-and-write
@@ -37,7 +74,7 @@ sbcl --noinform --load "$0" --eval '(generate-all)' --quit && exit
 
 (defun generate (file)
   (let ((dom (plump:make-root)))
-    (cl-markless:output (cl-markless:parse file T)
+    (cl-markless:output (cl-markless:parse file (make-instance 'cl-markless:parser :embed-types (list* 'youtube cl-markless:*default-embed-types*)))
                         :target dom
                         :format (make-instance 'org.shirakumo.markless.plump:plump
                                                :css (style)))
