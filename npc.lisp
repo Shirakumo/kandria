077ae1d1a27bca6ae734adcba2a4a33d85313e7a
Shinmera <shinmera@tymoon.eu>
2020-10-31 14:12:27 +0100

    Idles for all animatables.

--- npc.lisp
+++ npc.lisp
@@ -1,15 +1,7 @@
 (in-package #:org.shirakumo.fraf.kandria)
 
 (define-shader-entity npc (animatable dialog-entity profile)
-  ((idle-time :initform (random 15.0) :accessor idle-time)))
-
-(defmethod handle :after ((ev tick) (npc npc))
-  (case (state npc)
-    (:normal
-     (decf (idle-time npc) (dt ev))
-     (when (<= (idle-time npc) 0.0)
-       (setf (idle-time npc) (+ 7.0 (random 8.0)))
-       (setf (animation npc) 'idle)))))
+  ())
 
 (define-shader-entity fi (npc)
   ((name :initform 'fi)
