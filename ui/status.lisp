2921ea1487296757555198fc56ea2a12592068bc
Shinmera <shinmera@tymoon.eu>
2020-11-05 14:08:51 +0100

    Improve and fix status line rendering.

--- ui/status.lisp
+++ ui/status.lisp
@@ -1,7 +1,8 @@
 (in-package #:org.shirakumo.fraf.kandria)
 
 (defclass status-line (alloy:label*)
-  ((timeout :initarg :timeout :accessor timeout)))
+  ((timeout :initarg :timeout :accessor timeout)
+   (importance :initarg :importance :initform :normal :accessor importance)))
 
 (presentations:define-realization (ui status-line)
   ((:label simple:text)
@@ -9,8 +10,25 @@
    :font "PromptFont"
    :valign :top
    :halign :left
-   :size (alloy:un 12)
-   :pattern colors:white))
+   :size (ecase (importance alloy:renderable)
+           (:note (alloy:un 16))
+           (:normal (alloy:un 20))
+           (:important (alloy:un 26)))
+   :pattern (ecase (importance alloy:renderable)
+              (:note colors:white)
+              (:normal colors:white)
+              (:important colors:yellow))))
+
+(defmethod presentations:update-shape :around ((ui ui) (line status-line) shape))
+
+(defmethod alloy:suggest-bounds ((extent alloy:extent) (element status-line))
+  (alloy:extent (alloy:x extent)
+                (alloy:y extent)
+                500
+                (ecase (importance element)
+                  (:note 20)
+                  (:normal 26)
+                  (:important 32))))
 
 (defclass status-lines (panel)
   ())
@@ -18,21 +36,27 @@
 (defmethod initialize-instance :after ((panel status-lines) &key)
   (let ((layout (make-instance 'org.shirakumo.alloy.layouts.constraint:layout))
         (list (make-instance 'alloy:vertical-linear-layout)))
-    (alloy:enter list layout :constraints `((:left 10) (:top 100) (:size 500 1000)))
+    (alloy:enter list layout :constraints `((:left 20) (:top 100) (:size 500 1000)))
     (alloy:finish-structure panel layout NIL)))
 
-(defmethod alloy:enter ((string string) (panel status-lines) &key)
+(defmethod alloy:enter ((string string) (panel status-lines) &key (importance :normal))
   (let ((layout (alloy:index-element 0 (alloy:layout-element panel))))
-    (make-instance 'status-line :value string :timeout (+ (clock +world+) 4.0) :layout-parent layout)))
+    (make-instance 'status-line :value string :timeout (+ (clock +world+) 4.0)
+                                :importance importance :layout-parent layout)))
 
-(defun status (string &rest args)
+(defun status (importance &optional string &rest args)
+  (when (stringp importance)
+    (push string args)
+    (setf string importance)
+    (setf importance :normal))
   (let ((panel (find-panel 'status-lines)))
     (when panel
-      (alloy:enter (format NIL "~?" string args) panel))))
+      (alloy:enter (format NIL "~?" string args) panel :importance importance))))
 
 (defmethod handle ((ev tick) (panel status-lines))
   (let ((layout (alloy:index-element 0 (alloy:layout-element panel)))
         (clock (clock +world+)))
     (alloy:do-elements (element layout)
       (when (< (timeout element) clock)
-        (alloy:leave element layout)))))
+        (when (slot-boundp element 'alloy:layout-parent)
+          (alloy:leave element layout))))))
