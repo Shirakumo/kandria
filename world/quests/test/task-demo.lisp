9536cb174e508bc81a26a52c3f156fbc3ed206af
Tim White <timwhiteuk@gmail.com>
2020-11-20 13:06:14 +0000

    demo quest first pass on all dialogue

--- world/quests/test/task-demo.lisp
+++ world/quests/test/task-demo.lisp
@@ -3,14 +3,14 @@
  :description "Locate the android rumoured to live in the mountain caves"
  :invariant T
  :condition quest:all-complete
- :on-activate (fi-check pot-check tent-check sign-main-check sign-check fire-check corpse-check))
+ :on-activate (fi-check pot-check tent-check sign-main-check sign-check landslide-check fire-check beacon-check corpse-check cave-in-check android-check))
 (quest:interaction :name fi-check :interactable fi :dialogue "
 ~ fi
-| (:annoyed)Next time I'll bring my climbing gear.
+| (:annoyed)Next time I'll bring my own climbing gear.
 ~ player
 | Suit yourself. But this was your idea.
 ~ fi
-| Tracking the android was my idea. Talking to Harris was my idea. Clinging to your back while you scale a cliff face - (:shocked)definitely not my idea.
+| Tracking the android was my idea. Talking to Harris was my idea. Clinging to your back while you scale a vertical cliff face - (:shocked)definitely not my idea.
 ~ player
 | Well, we're here now. Should I proceed?
 ~ fi
@@ -26,7 +26,7 @@
 (quest:interaction :name tent-check :interactable tent :dialogue "
 ~ player
 | (There's no one inside, but the bedroll suggests it was recently occupied.)
-| (Does an android need a bedroll? I sleep standing up.)
+| (Does an android require a bedroll? I recharge standing up.)
 | ! eval (activate 'fi-tent)
 | ! eval (complete 'fi-check)
 ")
@@ -80,6 +80,28 @@
 ~ fi
 | A fact.
 | [has-more-dialogue Anything else? | Now get going.]")
+(quest:interaction :name landslide-check :interactable landslide :dialogue "
+~ player
+| (There was a landslide here... That's a memory I could do without.)
+| (And this is human blood. There must be another way around.)
+| ! eval (activate 'fi-landslide)
+| ! eval (complete 'fi-check)
+")
+; | ! eval (let (landslide true))
+(quest:interaction :name fi-landslide :interactable fi :dialogue "
+~ player
+| There's been a landslide in the cave. I think there might be bodies underneath.
+~ fi
+| That's not unusual in itself, but given everything else about this place...
+~ player
+| I hope the android isn't buried under there as well.
+~ fi
+| Androids are tougher than they look.
+~ player
+| I don't look tough?
+~ fi
+| <smiles> I didn't say that.
+| [has-more-dialogue Anything more? | Well this is awkward...]")
 (quest:interaction :name fire-check :interactable fire :dialogue "
 ~ player
 | (The fire is still warm. They left very recently.)
@@ -100,27 +122,54 @@
 ~ fi
 | You can handle yourself.
 | [has-more-dialogue Next? | Sayonara.]")
+(quest:interaction :name beacon-check :interactable beacon :dialogue "
+~ player
+| (How'd this get all the way up here?)
+| (Whoa. That's one helluva view across the desert. Somebody's thinking place?)
+| ! eval (complete 'fi-check)
+")
+
+
 (quest:interaction :name corpse-check :interactable corpse :dialogue "
 ~ player
-| The skeleton is clutching a notebook. (:skeptical)Excuse me, skeleton...
-| (:normal)\"I fucked up. I managed to narrowly escape the cave-in, but the tunnel shut behind me and now I'm stuck in this tiny cave.
-| \"No way in hell am I gonna be able to dig out of here. What the fuck am I gonna do?
-| \"I guess for now I should be happy I had my backpack with me. Enough to make a bed and a fire. I'll have to call for help tomorrow.
-| \"It's so damn cold in here. My supplies are now out and nobody's been answering my calls. I guess this is it.
-| \"Heh. I always knew things were fucked up, but I didn't think I'd end up like this. So pathetic.
-| \"For a while I was doing well for myself. Got yourself an entire crew together, eh, Harris?
-| \"Not much left of all of that now, huh. Fuck. If only I had listened to Sara.
-| \"Sara...\"-/-The ink trails off and runs out.
-| (:thinking)I wonder where the tunnel he came from used to lead.
-! eval (activate 'fi-corpse)")
-(quest:interaction :name fi-corpse :interactable fi :dialogue "
+| (Human remains. Dismembered, and picked clean...)
+| (Might have been an animal attack, or those rogue robots...)")
+; \/ query if already know of landslide var, and tweak text e.g. "This is the other side of the landslide"
+(quest:interaction :name cave-in-check :interactable cave-in :dialogue "
 ~ player
-| I found what appears to be the corpse of Harris. He got cut off from the rest of his group when a cave-in happened and starved to death.
+| (The cave collapsed here. I can see more bodies underneath.)
+| (Scratches in the rocks suggest combat occurred here - swords, gunfire, and finger nails...)")
+(quest:interaction :name android-check :interactable android :dialogue "
+~ player
+| (It's the android... It's been destroyed. Not even Catherine could fix this kind of damage.)
+| Hello, sister... What happened to you?
+| (More human remains... It appears you didn't go down without a fight.)
+| (The memory replay seems to be intact.)
+| Excuse me, sister... I just need to retrieve this... --- Thank you.
+| I'd better get this back to Fi.)
+! eval (activate 'fi-outcome)")
+(quest:interaction :name fi-outcome :interactable fi :dialogue "
 ~ fi
-| (:shocked)---(:annoyed) So there's nothing for us here, huh.
+| Is everything okay?
 ~ player
-| Looks like this was a bust, yeah. Sorry I dragged you all the way out here.
+| I found the android.
 ~ fi
-| (:unsure)That's...(:normal) It's alright. At least we know for sure there used to be someone here for quite a while.
+| Oh. I'm sorry. What happened?
 ~ player
-| (:thinking)I wonder where everyone else in this group went...")
+| They're offline and irreparable.
+| By the lay of the carnage in there, I think Harris and company ambushed them.
+| They fought, and the cave destabilised and collapsed. The humans were killed then and there - all but one.
+| I think the android then killed Harris - not just killed, but dismembered the poor bastard.
+| They spent her final seconds crawling back into their home, where they died.
+~ fi
+| -- I'm so sorry.
+| -- Perhaps they hoped to trick us, and seize you too.
+~ player
+| Perhaps.
+~ fi
+| I see you have the memory replay.
+~ player
+| *Holds the small metal chip aloft*
+~ fi
+| Let's go home. We'll see what it can tell us about the past.
+")	
