cca3891b99394f1698152de14249d0d68f026a9b
Shinmera <shinmera@tymoon.eu>
2020-12-10 10:48:42 +0100

    Minor

--- ui/pause.lisp
+++ ui/pause.lisp
@@ -1,12 +1,12 @@
 (in-package #:org.shirakumo.fraf.kandria)
 
 (defclass pause-list (alloy:vertical-linear-layout alloy:focus-list alloy:renderable)
-  ())
+  ((alloy:min-size :initform (alloy:size 100 50))))
 
 (presentations:define-realization (ui pause-list)
-  ((:background simple:rectangle)
+  ((:bg simple:rectangle)
    (alloy:margins)
-   :pattern (colored:color 0 0 0 0.5)))
+   :pattern (colored:color 0 0 0 0.75)))
 
 (defclass pause-menu (pausing-panel)
   ())
@@ -14,5 +14,7 @@
 (defmethod initialize-instance :after ((menu pause-menu) &key)
   (let ((layout (make-instance 'org.shirakumo.alloy.layouts.constraint:layout))
         (list (make-instance 'pause-list)))
-    (alloy:enter list layout :constraints `((:top 0) (:bottom 0) (:width 300) (:center :w)))
+    (alloy:enter list layout :constraints `((:top 0) (:bottom 0) (:width 400) (:center :w)))
+    (alloy:enter (make-instance 'alloy:button* :value "Resume" :on-activate (lambda () (hide menu))) list)
+    (alloy:enter (make-instance 'alloy:button* :value "Quit" :on-activate (lambda () (quit *context*))) list)
     (alloy:finish-structure menu layout list)))
