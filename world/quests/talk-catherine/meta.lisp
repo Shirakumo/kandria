49c11dec8e128bda9c2d9f526020399ed7cc98b6
Tim White <timwhiteuk@gmail.com>
2021-02-04 13:13:00 +0000

    removed test race quest from logbook added basic initial branching convo with catherine

--- world/quests/talk-catherine/meta.lisp
+++ world/quests/talk-catherine/meta.lisp
@@ -1,5 +1,5 @@
 (:identifier quest :version world-v0)
-(:name talk-catherine :author "Tim White"
- :title "Talk to Catherine" :description "Talk to Catherine."
+(:name settlement-emergency :author "Tim White"
+ :title "Emergency?" :description "Something's wrong - the one that reactivated me, Catherine, doesn't look happy. I'd better talk to her."
  :on-activate (talk-to-catherine)
  :tasks (#p"task-talk-catherine.lisp"))
--- world/quests/talk-catherine/task-talk-catherine.lisp
+++ world/quests/talk-catherine/task-talk-catherine.lisp
@@ -1,14 +1,28 @@
 (:name talk-to-catherine
  :title "Talk to Catherine"
- :description "I should check in with Catherine."
+ :description NIL
  :invariant T
  :condition NIL
  :on-activate (talk))
 (quest:interaction :name talk :interactable catherine :dialogue "
 ~ catherine
-| I need you to fix the water supply.
+| This isn't the welcome I was expecting.
 ~ player
-| Okay I will.
+- Is something wrong?
+  | ~ catherine
+  | Well, for a start where is everyone? I just reactivated an android!
+  | I thought everyone would be here to see you.
+- What were you expecting?
+  | ~ catherine
+  | I don't know... Though I just reactivated an android...
+  | I guess I thought everyone would be here to see you.
+- Is it me?
+  | ~ catherine
+  | You?... I suppose it could be.
+  | I mean, I think you're amazing - a working android from the old world!
+  | But not everyone has fond tales to tell about androids.
 ~ catherine
-| Thanks.")
+| We'd better go inside and talk to Jack.
+")
 ; ! eval (activate 'q1-water)
+; TODO mention stranger's name? Leave for jack to do?
