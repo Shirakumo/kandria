7bf03510bc0c40d094a034b8405e7ccb2a42ce89
Shinmera <shinmera@tymoon.eu>
2021-03-02 13:05:36 +0100

    Improve movement graph debug rendering

--- editor/move-to.lisp
+++ editor/move-to.lisp
@@ -1,41 +1,46 @@
 (in-package #:org.shirakumo.fraf.kandria)
 
-(define-shader-entity node-graph-visualizer (located-entity vertex-colored-entity vertex-entity standalone-shader-entity deferrer)
+(define-shader-entity node-graph-visualizer (vertex-colored-entity vertex-entity standalone-shader-entity deferrer)
   ())
 
 (defmethod initialize-instance :after ((visualizer node-graph-visualizer) &key graph)
   (setf (vertex-array visualizer) (generate-resources 'mesh-loader (node-graph-mesh (or graph (make-node-graph #() 0 0))))))
 
+(defmethod apply-transforms progn ((visualizer node-graph-visualizer)))
+
+(defmethod (setf node-graph) ((chunk chunk) (visualizer node-graph-visualizer))
+  (let* ((vao (vertex-array visualizer))
+         (mesh (node-graph-mesh (node-graph chunk) :offset (location chunk)))
+         (mesh (chunk-graph-mesh chunk :mesh mesh)))
+    (trial:replace-vertex-data vao mesh :update T)))
+
 (defmethod (setf node-graph) ((graph node-graph) (visualizer node-graph-visualizer))
   (let* ((vao (vertex-array visualizer))
-         (vbo (car (second (bindings vao))))
-         (ebo (first (bindings vao)))
          (mesh (node-graph-mesh graph)))
     (trial:replace-vertex-data vao mesh :update T)))
 
 (defmethod (setf path) (path (visualizer node-graph-visualizer))
   (when path
-    (setf (location visualizer) (vec 0 0))
     (let* ((vao (vertex-array visualizer))
-           (vbo (car (second (bindings vao))))
-           (ebo (first (bindings vao)))
            (mesh (path-mesh path)))
       (trial:replace-vertex-data vao mesh :update T))))
 
-(defun path-mesh (path)
-  (with-vertex-filling ((make-instance 'vertex-mesh :vertex-type 'colored-vertex :face-length 2))
+(defun path-mesh (path &key (mesh (make-instance 'vertex-mesh :vertex-type 'colored-vertex :face-length 2)))
+  (with-vertex-filling (mesh)
     (loop for prev = (second (first path)) then next
           for node in path
           for next = (second node)
           do (vertex :position (vxy_ prev) :color (vec 1 0 0 1))
              (vertex :position (vxy_ next) :color (vec 1 0 0 1)))))
 
-(defun node-graph-mesh (graph)
+(defun node-graph-mesh (graph &key (mesh (make-instance 'vertex-mesh :vertex-type 'colored-vertex :face-length 2))
+                                   (offset (vec 0 0)))
   (let ((w (node-graph-width graph))
         (h (node-graph-height graph)))
     (flet ((loc (x y)
-             (vec (* (- x (/ w 2) -0.5) +tile-size+) (* (- y (/ h 2) -0.5) +tile-size+) 0)))
-      (with-vertex-filling ((make-instance 'vertex-mesh :vertex-type 'colored-vertex :face-length 2))
+             (vec (+ (vx offset) (* (- x (/ w 2) -0.5) +tile-size+))
+                  (+ (vy offset) (* (- y (/ h 2) -0.5) +tile-size+)) 0)))
+      (with-vertex-filling (mesh)
         (do-nodes (x y graph)
           (dolist (node (node graph x y))
             (let ((color (etypecase node
@@ -47,6 +52,15 @@
               (vertex :position (loc x y) :color color)
               (vertex :position (loc (mod (move-node-to node) w) (floor (move-node-to node) w)) :color (v* color 0.1)))))))))
 
+(defun chunk-graph-mesh (chunk &key (mesh (make-instance 'vertex-mesh :vertex-type 'colored-vertex :face-length 2)))
+  (let ((graph (chunk-graph (region +world+))))
+    (flet ((loc (chunk node)
+             (vxy_ (chunk-node-vec chunk node))))
+      (with-vertex-filling (mesh)
+        (dolist (node (svref graph (chunk-graph-id chunk)))
+          (vertex :position (loc (chunk-node-from node) (chunk-node-from-node node)) :color (vec 1 1 0 1))
+          (vertex :position (loc (chunk-node-to node) (chunk-node-to-node node)) :color (vec 1 1 0 1)))))))
+
 (defmethod render :before ((viz node-graph-visualizer) (program shader-program))
   (apply-transforms viz))
 
@@ -57,8 +71,7 @@
 (defmethod label ((tool move-to)) "Movement")
 
 (defmethod (setf tool) :after ((tool move-to) (editor editor))
-  (setf (node-graph (visualizer tool)) (node-graph (entity tool)))
-  (setf (location (visualizer tool)) (location (entity tool)))
+  (setf (node-graph (visualizer tool)) (entity tool))
   (unless (find (visualizer tool) (alloy:elements (alloy:popups (alloy:layout-tree (unit 'ui-pass T)))))
     (alloy:enter (visualizer tool) (alloy:popups (alloy:layout-tree (unit 'ui-pass T)))
                  :w (width *context*) :h (height *context*))))
