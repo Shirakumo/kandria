bb7ff7869d448e195143fc520baeaa7f5888f8ea
Shinmera <shinmera@tymoon.eu>
2021-03-04 10:25:36 +0100

    Add initial state save and load stuff.

--- editor/editmenu.lisp
+++ editor/editmenu.lisp
@@ -16,6 +16,7 @@
 (alloy::define-subbutton (editmenu new) () (edit 'new-region T))
 (alloy::define-subbutton (editmenu save) () (edit 'save-region T))
 (alloy::define-subbutton (editmenu load) () (edit 'load-region T))
+(alloy::define-subbutton (editmenu save-init) ("Save Initial State") (edit 'save-initial-state T))
 (alloy:define-subcomponent (editmenu zoom) ((zoom (unit :camera T)) zoom-slider
                                             :range '(0.3 . 1.3) :step 0.05 :grid 0.05
                                             :ideal-bounds (alloy:extent 0 0 100 20)))
@@ -30,11 +31,11 @@
                                     :shapes (list (make-instance 'simple:filled-rectangle :bounds (alloy:margins)
                                                                                           :name :background))
                                     :style `((:background :pattern ,(colored:color 0.1 0.1 0.1))))
-  new save load zoom undo redo lighting time)
+  new save load save-init zoom undo redo lighting time)
 
 (alloy:define-subcontainer (editmenu focus :if-exists :supersede)
     (alloy:focus-list)
-  new save load zoom undo redo lighting time)
+  new save load save-init zoom undo redo lighting time)
 
 (defmethod initialize-instance :after ((menu editmenu) &key)
   (alloy:on alloy:value (value (alloy:representation 'lighting menu))
