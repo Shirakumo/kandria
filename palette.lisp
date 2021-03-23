a1d623c604df2e6ff5f0f24cf3714aeb89e2672d
Shinmera <shinmera@tymoon.eu>
2021-03-23 14:22:34 +0100

    Clean shit up, move resource precompilation into kandria rather than the kandria-data system.

---
+++ palette.lisp
@@ -0,0 +1,52 @@
+(in-package #:org.shirakumo.fraf.kandria)
+
+(define-shader-entity paletted-entity ()
+  ((palette :initarg :palette :initform (// 'kandria 'placeholder) :accessor palette
+            :type resource)
+   (palette-index :initarg :palette-index :initform 0 :accessor palette-index
+                  :type integer)))
+
+(defmethod stage :after ((entity paletted-entity) (area staging-area))
+  (stage (palette entity) area))
+
+(defmethod render :before ((entity paletted-entity) (program shader-program))
+  (gl:active-texture :texture4)
+  (gl:bind-texture :texture-2D (gl-name (palette entity)))
+  (setf (uniform program "palette") 4)
+  (setf (uniform program "palette_index") (palette-index entity)))
+
+(define-class-shader (paletted-entity :fragment-shader -1)
+  "uniform sampler2D palette;
+uniform int palette_index = 0;
+
+void main(){
+  if(color.r*color.b == 1 && color.g < 0.1){
+    color = texelFetch(palette, ivec2(color.g*255, palette_index), 0);
+  }
+}")
+
+(defun convert-palette (file palette)
+  (let* ((palette (pngload:data (pngload:load-file palette)))
+         (input (pngload:load-file file :flatten T))
+         (data (pngload:data input))
+         (y (1- (array-dimension palette 0))))
+    (flet ((find-color (r g b)
+             (loop for x from 0 below (array-dimension palette 1)
+                   do (when (and (= r (aref palette y x 0))
+                                 (= g (aref palette y x 1))
+                                 (= b (aref palette y x 2)))
+                        (return x)))))
+      (loop for i from 0 below (length data) by 4
+            for index = (when (< 0 (aref data (+ i 3)))
+                          (find-color (aref data (+ i 0))
+                                      (aref data (+ i 1))
+                                      (aref data (+ i 2))))
+            do (when index
+                 (setf (aref data (+ i 0)) 255)
+                 (setf (aref data (+ i 1)) index)
+                 (setf (aref data (+ i 2)) 255))))
+    (zpng:write-png (make-instance 'zpng:png :color-type :truecolor-alpha
+                                             :width (pngload:width input)
+                                             :height (pngload:height input)
+                                             :image-data data)
+                    file :if-exists :supersede)))
--- tile-data.lisp
+++ tile-data.lisp
@@ -1,6 +1,6 @@
 (in-package #:org.shirakumo.fraf.kandria)
 
-(defclass tile-data (multi-resource-asset file-input-asset)
+(defclass tile-data (compiled-generator multi-resource-asset file-input-asset)
   ((tile-types :initform () :accessor tile-types)))
 
 (defun parse-tile-types (types)
@@ -17,36 +17,42 @@
     (loop for (name . tiles) in types
           collect (list* name (mapcar #'parse-type-spec tiles)))))
 
-(defmethod generate-resources ((data tile-data) (path pathname) &key)
-  (with-kandria-io-syntax
-    (with-open-file (stream path)
-      (destructuring-bind (&key albedo absorption normal tile-types) (read stream)
-        (setf (tile-types data) (parse-tile-types tile-types))
-        (generate-resources 'image-loader (merge-pathnames albedo path)
-                            :resource (resource data 'albedo))
-        (generate-resources 'image-loader (merge-pathnames absorption path)
-                            :resource (resource data 'absorption))
-        (if normal
-            (generate-resources 'image-loader (merge-pathnames normal path)
-                                :resource (resource data 'normal))
-            (ensure-instance (resource data 'normal) 'texture
-                             :width (width (resource data 'albedo))
-                             :height (height (resource data 'albedo))
-                             :internal-format :rg8
-                             :pixel-format :rg
-                             :pixel-data (make-array (* 2 (width (resource data 'albedo)) (height (resource data 'albedo)))
-                                                     :element-type '(unsigned-byte 8) :initial-element 128)))
-        (list (resource data 'albedo)
-              (resource data 'absorption)
-              (resource data 'normal))))))
+(defmethod compile-resources ((data tile-data) (path pathname))
+  (destructuring-bind (&key source albedo absorption normal &allow-other-keys) (read-src path)
+    (let ((source (merge-pathnames source path))
+          (albedo (merge-pathnames albedo path))
+          (absorption (merge-pathnames absorption path))
+          (normal (merge-pathnames normal path)))
+      (when (recompile-needed-p (list albedo absorption normal)
+                                (list path source))
+        (v:info :kandria.resources "Compiling tileset from ~a..." source)
+        (aseprite source "--layer" "albedo" "--save-as" albedo)
+        (aseprite source "--layer" "absorption" "--save-as" absorption)
+        (aseprite source "--layer" "normal" "--save-as" normal)))))
+
+(defmethod generate-resources ((data tile-data) (path pathname) &key compile)
+  (destructuring-bind (&key source albedo absorption normal tile-types) (read-src path)
+    (let ((albedo (merge-pathnames albedo path))
+          (absorption (merge-pathnames absorption path))
+          (normal (merge-pathnames normal path)))
+      (setf (tile-types data) (parse-tile-types tile-types))
+      (generate-resources 'image-loader albedo
+                          :resource (resource data 'albedo))
+      (generate-resources 'image-loader absorption
+                          :resource (resource data 'absorption))
+      (generate-resources 'image-loader normal
+                          :resource (resource data 'normal))
+      (list (resource data 'albedo)
+            (resource data 'absorption)
+            (resource data 'normal)))))
 
 (defmethod resource ((data tile-data) (name (eql T)))
   (resource data 'albedo))
 
 (defmethod notify:files-to-watch append ((asset tile-data))
-  (list (make-pathname :type "ase" :defaults (input* asset))))
+  (list (merge-pathnames (getf (read-src (input* asset)) :source) (input* asset))))
 
 (defmethod notify:notify :before ((asset tile-data) file)
   (when (string= "ase" (pathname-type file))
     (sleep 1)
-    (ql:quickload :kandria-data)))
+    (compile-resources asset T)))
