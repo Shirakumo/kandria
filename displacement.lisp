52502325936857c0016f745a5f3c29f32a40db1e
Shinmera <shinmera@tymoon.eu>
2021-03-29 14:22:29 +0200

    Fix displacement display and reset when loading state.

--- displacement.lisp
+++ displacement.lisp
@@ -1,38 +1,49 @@
 (in-package #:org.shirakumo.fraf.kandria)
 
-(define-asset (kandria displacement) image
-    #p"shockwave-2.png")
-
-(define-shader-entity displacer (located-entity listener)
-  ((texture :initform (// 'kandria 'displacement) :reader texture)
-   (clock :initform 0f0 :accessor clock)
-   (lifetime :initform 1f0 :initarg :lifetime :accessor lifetime)
+(define-shader-entity displacer ()
+  ((texture :initarg :texture :reader texture)
    (strength :initform 1f0 :initarg :strength :accessor strength)))
 
 (defmethod object-renderable-p ((displacer displacer) (pass shader-pass)) NIL)
 
-(defmethod handle ((ev tick) (displacer displacer))
-  (incf (clock displacer) (dt ev))
-  (when (< (lifetime displacer) (clock displacer))
-    (leave displacer T)
-    (remove-from-pass displacer (unit 'displacement-render-pass T))))
-
-(defmethod apply-transforms progn ((displacer displacer))
-  (let ((tt (1+ (* 10 (clock displacer)
-                   (/ (1+ (strength displacer)) 2)
-                   (/ (lifetime displacer))))))
-    (scale-by tt tt 1)))
-
 (defmethod render ((displacer displacer) (program shader-program))
   (setf (uniform program "model_matrix") (model-matrix))
-  (setf (uniform program "effect_strength") (* (/ (strength displacer) 10f0)
-                                               (- 1 (/ (clock displacer) (lifetime displacer)))))
+  (setf (uniform program "effect_strength") (strength displacer))
   (let ((vao (// 'kandria '16x)))
     (gl:active-texture :texture0)
     (gl:bind-texture :texture-2d (gl-name (texture displacer)))
     (gl:bind-vertex-array (gl-name vao))
     (%gl:draw-elements (vertex-form vao) (size vao) :unsigned-int 0)
     (gl:bind-vertex-array 0)))
+
+(define-shader-entity shockwave (displacer located-entity listener)
+  ((texture :initform (// 'kandria 'shockwave))
+   (clock :initform 0f0 :accessor clock)
+   (lifetime :initform 0.3 :initarg :lifetime :accessor lifetime)
+   (initial-strength :initform 1f0 :initarg :strength :accessor initial-strength)))
+
+(defmethod handle ((ev tick) (displacer shockwave))
+  (incf (clock displacer) (dt ev))
+  (setf (strength displacer)
+        (* (/ (initial-strength displacer) 10f0)
+           (- 1 (/ (clock displacer) (lifetime displacer)))))
+  (when (< (lifetime displacer) (clock displacer))
+    (leave displacer T)
+    (remove-from-pass displacer (unit 'displacement-render-pass T))))
+
+(defmethod apply-transforms progn ((displacer shockwave))
+  (let ((tt (1+ (* 10 (clock displacer)
+                   (/ (1+ (initial-strength displacer)) 2)
+                   (/ (lifetime displacer))))))
+    (scale-by tt tt 1)))
+
+(define-shader-entity scanline (displacer transformed)
+  ((texture :initform (// 'kandria 'scanline))
+   (strength :initform 1.0)))
+
+(defmethod apply-transforms progn ((displacer scanline))
+  (translate (vxy_ (location (unit :camera T))))
+  (scale-by 40 25 1))
 
 (define-shader-pass displacement-render-pass (single-shader-scene-pass)
   ((displacement-map :port-type output :attachment :color-attachment0
@@ -41,7 +52,8 @@
 
 (defmethod stage :after ((pass displacement-render-pass) (area staging-area))
   (stage (// 'kandria '16x) area)
-  (stage (// 'kandria 'displacement) area))
+  (stage (// 'kandria 'scanline) area)
+  (stage (// 'kandria 'shockwave) area))
 
 (defmethod object-renderable-p ((renderable renderable) (pass displacement-render-pass)) NIL)
 (defmethod object-renderable-p ((displacer displacer) (pass displacement-render-pass)) T)
@@ -90,7 +102,7 @@ out vec4 color;
 
 void main(){
   vec3 data = texture(displacement_map, tex_coord).rgb;
-  vec2 displacement = (data.xy - 0.5)/10.0;
-  vec3 previous = texture(previous_pass, tex_coord+displacement*data.z*10).rgb;
+  vec2 displacement = (data.xy - 0.5);
+  vec3 previous = texture(previous_pass, tex_coord+displacement*data.z).rgb;
   color = vec4(previous, 1);
 }")
