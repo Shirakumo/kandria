5b2562c29559e8c06d4cf656b73a24189090fa93
Tim White <timwhiteuk@gmail.com>
2021-04-08 12:35:44 +0100

    trader repeat shop buying (not selling, yet)

--- world/quests/trader-repeat/meta.lisp
+++ world/quests/trader-repeat/meta.lisp
@@ -2,4 +2,6 @@
 (:name trader-repeat :author "Tim White"
  :title "Trade" :description "If I want to trade items, I should find Sahil in the Midwest Market, beneath the Hub."
  :on-activate (trade-trader)
- :tasks (#p"trade-trader.lisp"))
+ :tasks (#p"trade-trader.lisp")
+ :variables ((small-health-qty 10) (medium-health-qty 5) (large-health-qty 2))
+)
--- world/quests/trader-repeat/trade-trader.lisp
+++ world/quests/trader-repeat/trade-trader.lisp
@@ -6,6 +6,8 @@
  :on-activate (trade-with-trader)
  :on-complete NIL)
  
+ ;; TODO Sahil currently has limited stock in this act - it will not refresh until the next act, so once it's gone it's gone (set in this quest's meta)
+ ;; TODO use global vars instead of magic numbers for cost of packs
 (quest:interaction :name trade-with-trader :interactable trader :repeatable T :dialogue "
 ~ trader
 | Assalam Alaikum!
@@ -15,15 +17,66 @@
 | | [? Have you been fighting, Stranger? | Something's different - you're missing your usual refined appearance. | Let me guess - you've been pounding rogues again?]
 |?
 | | [? Though I think you've seen better days... | You look like you could really use my help today. | You look like you've been dragged throgh the desert backwards... | Forgive me for prying, but you're all scratched and scuffed - anything I can do?]
-! label main
+! label shop
+~ player
 - I'd like to trade.
   ~ trader
   | [? That's what I like to hear! | Yes, sir! | And so would I! | That's the spirit! | You got it.]
-  | p/h Open shop UI
-  < main
-- Can we talk.
+  ~ player
+  - //Buy//
+    ! label buy
+    ~ player
+    - //Small HP (I have {(item-count 'small-health-pack)}) - 4$//
+      ? (and (<= 4 (item-count 'parts)) (< 0 (var 'small-health-qty)))
+      | ! eval (retrieve 'parts 4)
+      | ! eval (store 'small-health-pack 1)
+      | ! eval (setf (var 'small-health-qty) (- (var 'small-health-qty) 1))
+      | ? (= 0 (var 'small-health-qty))
+      | | ~ trader
+      | | < last-one
+      | < buy
+      |? (> 4 (item-count 'parts))
+      | < cannot-afford
+      |?
+      | < out-of-stock
+    - //Medium HP (I have {(item-count 'medium-health-pack)}) - 10$//
+      ? (and (<= 10 (item-count 'parts)) (< 0 (var 'medium-health-qty)))
+      | ! eval (retrieve 'parts 10)
+      | ! eval (store 'medium-health-pack 1)
+      | ! eval (setf (var 'medium-health-qty) (- (var 'medium-health-qty) 1))
+      | ? (= 0 (var 'medium-health-qty))
+      | | ~ trader
+      | | < last-one
+      | < buy
+      |? (> 10 (item-count 'parts))
+      | < cannot-afford
+      |?
+      | < out-of-stock
+    - //Large HP (I have {(item-count 'large-health-pack)}) - 20$//
+      ? (and (<= 20 (item-count 'parts)) (< 0 (var 'large-health-qty)))
+      | ! eval (retrieve 'parts 20)
+      | ! eval (store 'large-health-pack 1)
+      | ! eval (setf (var 'large-health-qty) (- (var 'large-health-qty) 1))
+      | ? (= 0 (var 'large-health-qty))
+      | | ~ trader
+      | | < last-one
+      | < buy
+      |? (> 20 (item-count 'parts))
+      | < cannot-afford
+      |?
+      | < out-of-stock
+    - I'm done.
+      < shop
+  - //Sell//
+    ! label sell
+    | p/h selling UI
+    < shop
+  - I changed my mind.
+    < changed-mind
+- Can we talk.  
   ~ trader
   | [? Of course, habeebti - always. | We can indeed. | What's on your mind? | I love to chat.]
+  ! label talk
   ? (not (complete-p 'q4-find-allies))
   | - What's your story?
   |   ~ trader
@@ -31,14 +84,14 @@
   |   | I used to hang with the Wraw too, believe it or not.
   |   | I got out too, only with my caravan rather than a vendetta.
   |   | And now I tour the settlements, trading, making ends meet - and making things too!
-  |   < main
+  |   < talk
   | - What do you make of this place?
   |   ~ trader
   |   | The Noka? They're a good bunch, what can I say?
   |   | Fi's a good person, which is rare in these parts.
   |   | They broke out on their own, had enough of that Wraw bullshit.
   |   | Can't blame 'em. It was brave. It might also prove stupid though, we'll see.
-  |   < main
+  |   < talk
   | - Catherine said you were later than expected...
   |   ~ trader
   |   | Yeah, those damn rogues were prowling about.
@@ -50,7 +103,51 @@
   |   | Well no other alugud is going to do it for me!
   |   | I used to have an ox, believe or not... Ha, an ox, in these parts! It's hard to imagine.
   |   | Didn't last long after the wolves got at her throat though. Poor Celina.
-  |   < main
+  |   < talk
+  | - I changed my mind.
+  |   < changed-mind
+- I need to go.
+! label leave
+~ trader
+| [? See you later habeebti. | You take it easy. | Goodbye for now. | Take care. Masalamah! | Goodbye! And if you ever change your mind about parting with that sword of yours... I know, I know.]
+
+# last-one
+~ trader
+| That was the last of my stock on that item, for now.
+< buy
+
+# cannot-afford
+~ trader
+| Oh, looks like you can't afford that one. Sorry!
+< buy
+
+# out-of-stock
+~ trader
+| All out of stock on that one I'm afriad. Sorry, habeebti.
+< buy
+
+# changed-mind
+~ trader
+| [? Happens to the best of us. | As you wish. | Don't worry about it.]
+< shop
+")
+;; TODO: flesh out with Sahil questions relevant to current plot points - confidente, as a fellow outsider?
+
+#| TODO: when get scrolling options, add to buy menu:
+    - I want to sell.
+      < sell
+    - I'm done.
+      < shop
+    - //I need to go.//
+      < leave
+
+Also leave to Talk options
+  | - I need to go.
+  |   < leave
+|#
+
+#| TODO when get scrolling options, restore to talk menu:
+
   | - Do you know about finding a computer?
   |   ~ trader
   |   | A computer? Now there's a word you don't hear anymore.
@@ -62,12 +159,13 @@
   |     | You betcha! Boy do I miss the internet.
   |   - They were a new artform, sadly lost.
   |     ~ trader
-  |     | Whoa, were you a game developer in a past life or something?
+  |     | Well said, Stranger. Well said.
   |   - They used similar technology to my own. I admired that.
   |     ~ trader
   |     | Indeed, there was a lot to admire - especially for a tech-head like me.
   |   ~ trader
   |   | But no, no one told me anything about a computer. Which is good, because working ones are impossible to find.
+  |   < talk
   | - Do you like androids?
   |   ~ trader
   |   | Ah, you've had a warm welcome, have you?
@@ -79,28 +177,28 @@
   |   ~ trader
   |   | I don't know...
   |   | But what I do know is humanity could stand to take a good long look in the mirror.
-  |   < main
-  | - I changed my mind.
-  |   < changed-mind
-- I need to go.
-~ trader
-| [? See you later habeebti. | You take it easy. | Goodbye for now. | Take care. Masalamah! | Goodbye! And if you ever change your mind about parting with that sword of yours... I know, I know.]
+  |   < talk
 
-# changed-mind
-~ trader
-| [? Happens to the best of us. | As you wish. | Don't worry about it.]
-< main
-")
-;; TODO: open shop UI
-;; TODO: flesh out with Sahil questions relevant to current plot points - confidente, as a fellow outsider?
+|#
 
 #|
-- Take a small health pack for 2 scrap parts
-  ! eval (store 'small-health-pack 1)
-- Take a medium health pack for 5 scrap parts
-  ! eval (store 'medium-health-pack 1)
-- Take a large health pack for 10 scrap parts
-  ! eval (store 'large-health-pack 1)
+ORIGINAL IMPLEMENTATION IDEA FOR BUY MENU - works
+    # buy
+    - [(and (<= 4 (item-count 'parts)) (< 0 (var 'small-health-qty))) //Buy a small health pack for 4 scrap parts// | I can't afford 4 scrap parts for a small health pack.|] (stock: (var 'small-health-qty))
+      ! eval (retrieve 'parts 4)
+      ! eval (store 'small-health-pack 1)
+      ! eval (setf (var 'small-health-qty) (- (var 'small-health-qty) 1))
+      < buy
+    - [(and (<= 10 (item-count 'parts)) (< 0 (var 'medium-health-qty))) //Buy a medium health pack for 10 scrap parts// | I can't afford 10 scrap parts for a medium health pack.|] (stock: (var 'medium-health-qty))
+      ! eval (retrieve 'parts 10)
+      ! eval (store 'medium-health-pack 1)
+      ! eval (setf (var 'medium-health-qty) (- (var 'medium-health-qty) 1))
+      < buy
+    - [(and (<= 20 (item-count 'parts)) (< 0 (var 'large-health-qty))) //Buy a large health pack for 20 scrap parts// | I can't afford 20 scrap parts for a large health pack.|] (stock: (var 'large-health-qty))
+      ! eval (retrieve 'parts 20)
+      ! eval (store 'large-health-pack 1)
+      ! eval (setf (var 'large-health-qty) (- (var 'large-health-qty) 1))
+      < buy
 |#
 
 #| TODO:
