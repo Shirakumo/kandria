9809df82d4ffc7189a6e5f5ba21a2be2d59d14d4
Shinmera <shinmera@tymoon.eu>
2021-04-17 17:36:42 +0200

    Set default steam branch to developer

--- releases/release.lisp
+++ releases/release.lisp
@@ -1,5 +1,6 @@
 (org.shirakumo.fraf.trial.release:configure
- :build (:features (:kandria-release))
- :itch (:user "Shinmera")
- :steam (:user "shirakumo_org")
- :system "kandria")
+  :build (:features (:kandria-release))
+  :itch (:user "Shinmera")
+  :steam (:branch "developer"
+          :user "shirakumo_org")
+  :system "kandria")
