31df8c6d0b41717408b5e662909a8b719af4bb35
Shinmera <shinmera@tymoon.eu>
2021-08-27 16:26:14 +0200

    Minor tweaks

--- ui/load-screen.lisp
+++ ui/load-screen.lisp
@@ -53,13 +53,14 @@
 (defclass load-screen (fullscreen-panel loader alloy:observable-object)
   ((progress :initform 0)
    (text :accessor text)
+   (cold-boot :accessor cold-boot)
    (last-time :accessor last-time)))
 
 (defmethod initialize-instance :after ((panel load-screen) &key)
   (let ((layout (make-instance 'load-screen-layout))
         (bar (alloy:represent (slot-value panel 'progress) 'progress-bar))
         (text (setf (text panel) (make-instance 'load-text :value ""))))
-    (alloy:enter text layout :constraints `((:left 20) (:top 20) (:bottom 100) (:right 20)))
+    (alloy:enter text layout :constraints `((:left 20) (:top 20) (:bottom 0) (:right 20)))
     (alloy:enter bar layout :constraints `((:bottom 50) (:center :w) (:width 500) (:height 30)))
     (alloy:finish-structure panel layout NIL)))
 
@@ -68,6 +69,7 @@
   (declare (ignore unload))
   (if show-screen
       (let ((*show-load-screen* T))
+        (setf (cold-boot loader) cold)
         (setf (alloy:value (text loader))
               (if cold (@ load-screen-new-game) (@ load-screen-load-game)))
         (setf (slot-value loader 'progress) 0)
@@ -90,6 +92,8 @@
         (let ((ui (unit 'ui-pass +world+)))
           (ignore-errors (render ui T))
           (blit-to-screen ui)))
-      (when (< (* internal-time-units-per-second 1/30) (- (get-internal-real-time) (last-time loader)))
+      (when (if (cold-boot loader)
+                (< (* internal-time-units-per-second 1/60) (- (get-internal-real-time) (last-time loader)))
+                (< (* internal-time-units-per-second 1/30) (- (get-internal-real-time) (last-time loader))))
         (setf (last-time loader) (get-internal-real-time))
         (swap-buffers *context*)))))
