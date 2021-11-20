43e74645ce56073d401c17fdfae61a1b18f3440a
Shinmera <shinmera@tymoon.eu>
2021-11-20 12:43:36 +0100

    Fix blending of layers with transparency

--- chunk.lisp
+++ chunk.lisp
@@ -250,10 +250,10 @@ void main(){
   float a = texelFetch(absorption, tile_xy, 0).r;
   vec2 n = texelFetch(normal, tile_xy, 0).rg-0.5;
   if(abs(n.x) < 0.1 && abs(n.y) < 0.1)
-    color = apply_lighting_flat(color, vec2(0), 1-a, world_pos) * visibility;
+    color = apply_lighting_flat(color, vec2(0), 1-a, world_pos);
   else
-    color = apply_lighting(color, vec2(0), 1-a, normalize(n), world_pos) * visibility;
-  
+    color = apply_lighting(color, vec2(0), 1-a, normalize(n), world_pos);
+  color.a *= visibility;
 }")
 
 (define-shader-entity chunk (shadow-caster layer solid ephemeral collider creatable)
