e2d005572318c58aaca4c002f625d863c060bcc6
Shinmera <shinmera@tymoon.eu>
2021-11-26 15:03:47 +0100

    fix chunk movement

--- chunk.lisp
+++ chunk.lisp
@@ -357,6 +357,7 @@ void main(){
     ;; NOTE: Can't use region bvh here as we want to reach everything even things that aren't colliders
     (for:for ((entity over (region +world+)))
       (when (and (not (eql 'layer (type-of entity)))
+                 (not (eql 'chunk (type-of entity)))
                  (contained-p entity chunk))
         (nv+ (location entity) diff)))
     (call-next-method)))
