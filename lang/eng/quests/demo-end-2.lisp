e3ea8b864d3ab811e36d58050754097b7a57a1c9
Tim White <timwhiteuk@gmail.com>
2022-02-08 14:58:12 +0000

    demo end quest changed to sequence and added delay

--- lang/eng/quests/demo-end-2.lisp
+++ lang/eng/quests/demo-end-2.lisp
@@ -1,12 +1,10 @@
 ;; -*- mode: poly-dialog; -*-
 (in-package #:org.shirakumo.fraf.kandria)
 
-(quest:define-quest (kandria demo-end-2)
+(define-sequence-quest (kandria demo-end-2)
   :author "Tim White"
   :title "Demo End"
   :visible NIL
-  :on-activate (show-screen)
-  (show-screen
-   :title ""
-   :on-activate (activate)
+  (:wait 1.0)
+  (:eval
    (:action activate (show-panel 'end-screen))))
