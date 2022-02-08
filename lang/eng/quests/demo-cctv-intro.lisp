6cee54f36828fa04829b8fc777cbf0cb16bb6413
Tim White <timwhiteuk@gmail.com>
2022-02-08 14:59:14 +0000

    demo trigger names fix

--- lang/eng/quests/demo-cctv-intro.lisp
+++ lang/eng/quests/demo-cctv-intro.lisp
@@ -32,8 +32,8 @@
 | \"Report back to Innis\"(orange) when you have news - by then we'll probably be \"up in the control room\"(orange).
 | Good luck.
 ! eval (setf (nametag (unit 'innis)) (@ innis-nametag))
-! eval (activate (unit 'move-semis-1))
-! eval (activate (unit 'move-semis-2))
+! eval (activate (unit 'demo-move-semis-1))
+! eval (activate (unit 'demo-move-semis-2))
 ")
   (:eval
    :on-complete (demo-cctv)))
