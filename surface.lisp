1f0887776d306f9b4b201c83f0b9dda4575ab399
Shinmera <shinmera@tymoon.eu>
2022-02-08 10:58:52 +0100

    Re-add the missing aabb function

--- surface.lisp
+++ surface.lisp
@@ -68,3 +68,39 @@
 (defmethod bsize ((block block))
   #.(vec2 (/ +tile-size+ 2) (/ +tile-size+ 2)))
 
+(defun aabb (seg-pos seg-vel aabb-pos aabb-size)
+  (declare (type vec2 seg-pos seg-vel aabb-pos aabb-size))
+  (declare (optimize speed))
+  (let* ((scale-x (if (~= 0 (vx2 seg-vel) 0.00001) 1000000f0 (/ (vx2 seg-vel))))
+         (scale-y (if (~= 0 (vy2 seg-vel) 0.00001) 1000000f0 (/ (vy2 seg-vel))))
+         (sign-x (if (<= 0. (vx2 seg-vel)) +1. -1.))
+         (sign-y (if (<= 0. (vy2 seg-vel)) +1. -1.))
+         (near-x (* (- (vx2 aabb-pos) (* sign-x (vx2 aabb-size)) (vx2 seg-pos)) scale-x))
+         (near-y (* (- (vy2 aabb-pos) (* sign-y (vy2 aabb-size)) (vy2 seg-pos)) scale-y))
+         (far-x (* (- (+ (vx2 aabb-pos) (* sign-x (vx2 aabb-size))) (vx2 seg-pos)) scale-x))
+         (far-y (* (- (+ (vy2 aabb-pos) (* sign-y (vy2 aabb-size))) (vy2 seg-pos)) scale-y)))
+    (unless (or (< far-y near-x)
+                (< far-x near-y))
+      (let ((t-near (max near-x near-y))
+            (t-far (min far-x far-y)))
+        (when (and (< t-near 1)
+                   (< 0 t-far))
+          (let ((normal (cond ((< t-near 0)
+                               (let ((dist (tv- seg-pos aabb-pos)))
+                                 (if (< (abs (vy2 dist)) (abs (vx2 dist)))
+                                     (tvec (signum (vx2 dist)) 0)
+                                     (tvec 0 (signum (vy2 dist))))))
+                              ((< near-y near-x)
+                               (tvec (- sign-x) 0))
+                              (T
+                               (tvec 0 (- sign-y))))))
+            (unless (= 0 (v. normal seg-vel))
+              ;; KLUDGE: This test is necessary in order to ignore edges
+              ;;         that seem to stick out of the blocks. I have no idea why.
+              (unless (if (/= 0 (vy2 normal))
+                          (<= (vx2 aabb-size) (abs (- (vx2 aabb-pos) (vx2 seg-pos))))
+                          (<= (vy2 aabb-size) (abs (- (vy2 aabb-pos) (vy2 seg-pos)))))
+                (make-hit NIL
+                          aabb-pos
+                          t-near
+                          normal)))))))))
