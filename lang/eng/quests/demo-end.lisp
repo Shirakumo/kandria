29cb05351cc794d80ecd3b19856a358c2af390f4
Shinmera <shinmera@tymoon.eu>
2022-02-28 11:02:58 +0100

    Adjust demo end screen

--- lang/eng/quests/demo-end.lisp
+++ lang/eng/quests/demo-end.lisp
@@ -1,17 +1,10 @@
 ;; -*- mode: poly-dialog; -*-
 (in-package #:org.shirakumo.fraf.kandria)
 
-(quest:define-quest (kandria demo-end)
+(define-sequence-quest (kandria demo-end)
   :author "Nicolas Hafner"
   :title "Demo end"
   :visible NIL
-  :on-activate (wait-for-completion)
-  (wait-for-completion
-   :title ""
-   :condition (complete-p 'sq1-leaks 'sq2-mushrooms 'sq3-race)
-   :on-complete (show-screen))
-  (show-screen
-   :title ""
-   :on-activate (activate)
-   (:action activate (show-panel 'end-screen))))
-
+  (:complete (q2-seeds q3-new-home))
+  (:wait 5.0)
+  (:eval (show-panel 'end-screen)))
