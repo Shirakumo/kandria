b99d64458f7539f8d619e2386b9db4669821ca13
Tim White <timwhiteuk@gmail.com>
2022-04-04 17:09:35 +0100

    loc comment

--- lang/eng/quests/world.lisp
+++ lang/eng/quests/world.lisp
@@ -472,6 +472,7 @@
 | \"It's a temporary engineers' camp. They've got food enough to be self-sufficient, at least for a while.\"(light-gray, italic)
 ")
 
+  ;; the caved-in tunnel that trapped the Semi Sisters engineers. A different line plays depending on whether or not the debris has been cleared yet, and whether or not the player knows about the trapped engineers quests yet
   (:interaction engineers-wall
    :interactable lore-engineers-wall
    :repeatable T
