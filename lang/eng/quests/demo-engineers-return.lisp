1aba7c67c00aad8652eebb5b8211ffa682eb64e5
Tim White <timwhiteuk@gmail.com>
2022-04-05 16:24:09 +0100

    text improve

--- lang/eng/quests/demo-engineers-return.lisp
+++ lang/eng/quests/demo-engineers-return.lisp
@@ -3,13 +3,14 @@
 
 (define-sequence-quest (kandria demo-engineers-return)
   :author "Tim White"
-  :title "Demo Engineers Return"
+  :title "Engineers Return"
   :visible NIL
   (:wait 1.5)
   (:interact (player :now T)
   "
 ~ player
-| \"That's the \"tunnel cleared\"(orange). I should \"report to Innis in the Semis control room\"(orange).\"(light-gray, italic)
+| \"That's the \"tunnel cleared\"(orange). Now the Semis should be able to reach their engineers.\"(light-gray, italic)
+| \"I should \"report to Innis in the Semis control room\"(orange).\"(light-gray, italic)
 ! eval (setf (location 'innis) (location 'innis-main-loc))
 ! eval (setf (direction 'innis) 1)
 ! eval (activate (find-task 'demo-engineers 'task-return-engineers))
