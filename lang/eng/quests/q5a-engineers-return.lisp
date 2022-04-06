88147b5162024035b092ce8abad90b807f52817d
Tim White <timwhiteuk@gmail.com>
2022-04-06 14:10:04 +0100

    code style for engineers moving - removed unit

--- lang/eng/quests/q5a-engineers-return.lisp
+++ lang/eng/quests/q5a-engineers-return.lisp
@@ -11,9 +11,9 @@
 ~ player
 | \"That's the \"tunnel cleared\"(orange). Now the Semis should be able to reach their engineers.\"(light-gray, italic)
 | \"I should \"report back to Innis in the Semis control room\"(orange).\"(light-gray, italic)
-! eval (move-to 'engineer-home-1 (unit 'semi-engineer-1))
-! eval (move-to 'engineer-home-2 (unit 'semi-engineer-2))
-! eval (move-to 'engineer-home-3 (unit 'semi-engineer-3))
+! eval (move-to 'engineer-home-1 'semi-engineer-1)
+! eval (move-to 'engineer-home-2 'semi-engineer-2)
+! eval (move-to 'engineer-home-3 'semi-engineer-3)
 ! eval (activate (find-task 'q5a-rescue-engineers 'task-return-engineers))
 ! eval (deactivate (find-task 'q5a-rescue-engineers 'task-reminder))
 "))
