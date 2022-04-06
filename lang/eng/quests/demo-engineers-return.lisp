a25e57ef513dd0c6e47252c30ec3b263e3009b4d
Tim White <timwhiteuk@gmail.com>
2022-04-06 13:39:23 +0100

    move some engineers home when wall is cleared

--- lang/eng/quests/demo-engineers-return.lisp
+++ lang/eng/quests/demo-engineers-return.lisp
@@ -13,5 +13,8 @@
 | \"I should \"report to Innis in the Semis control room\"(orange).\"(light-gray, italic)
 ! eval (setf (location 'innis) (location 'innis-main-loc))
 ! eval (setf (direction 'innis) 1)
+! eval (move-to 'engineer-home-1 (unit 'semi-engineer-1))
+! eval (move-to 'engineer-home-2 (unit 'semi-engineer-2))
+! eval (move-to 'engineer-home-3 (unit 'semi-engineer-3))
 ! eval (activate (find-task 'demo-engineers 'task-return-engineers))
 "))
