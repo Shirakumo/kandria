fe0f123d3281f8bde6e0801ceee9ab8707da1cb0
Shinmera <shinmera@tymoon.eu>
2022-04-11 20:15:22 +0200

    Better cheats display

--- ui/cheats.lisp
+++ ui/cheats.lisp
@@ -1,22 +1,38 @@
 (in-package #:org.shirakumo.fraf.kandria)
 
+(defclass cheat-label (label)
+  ((markup :initform () :accessor markup)
+   (alloy:value :initform "")))
+
+(presentations:define-update (ui cheat-label)
+  (:label
+   :halign :middle
+   :markup (markup alloy:renderable)))
+
 (defclass cheat-panel (pausing-panel menuing-panel)
   ())
 
 (defmethod initialize-instance :after ((panel cheat-panel) &key)
   (let ((layout (make-instance 'big-prompt-layout))
-        (label (make-instance 'label :value "" :style `((:label :halign :middle)))))
+        (label (make-instance 'cheat-label)))
     (alloy:enter label layout :constraints `((:fill)))
     (alloy:on alloy:exit (label)
       (hide panel))
     (alloy:finish-structure panel layout label)))
 
 (defmethod handle ((ev text-entered) (panel cheat-panel))
-  (let ((longest 0) (text ""))
+  (let ((longest 0)
+        (text ""))
     (loop for cheat in *cheat-codes*
           for i = (cheat-idx cheat)
           for code = (cheat-code cheat)
-          do (when (< longest i)
-               (setf longest i)
-               (setf text (subseq code 0 i))))
-    (setf (alloy:value (alloy:focus-element panel)) text)))
+          do (cond ((< longest i)
+                    (setf longest i)
+                    (setf text code))
+                   ((= 0 i))
+                   ((= longest i)
+                    (setf text (format NIL "~a~%~a" text code)))))
+    (setf (alloy:value (alloy:focus-element panel)) text)
+    (setf (markup (alloy:focus-element panel))
+          `((0 ,longest (:color ,colors:white))
+            (,longest ,(length text) (:color ,colors:gray))))))
