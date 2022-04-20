66e1deafe0247a290c1b7a08e0e0fa15d11429b0
Tim White <timwhiteuk@gmail.com>
2022-04-20 12:07:58 +0100

    q11 dialogue tweaks

--- lang/eng/quests/q11-intro.lisp
+++ lang/eng/quests/q11-intro.lisp
@@ -16,20 +16,20 @@
   | (:happy)On the contrary.
 - Am I a free agent now?
   ~ fi
-  | (:happy)You're always a free agent. (:normal)But I have an important mission for you.
+  | (:happy)You're always a free agent. (:normal)But I have an important task for you.
 ~ fi
-| Innis was right about the Cerebat takeover, and you've come to know her better than anyone.
-| And if the Wraw have marched as far as the Cerebats, then the Semi Sisters are next.
-| (:annoyed)Now is not the time for division - the Wraw are a threat to them as much as they are to us.
-| (:normal)Use whatever sway you have with \"Innis and Islay\"(orange) and \"ask them to stand with us\"(orange).
+| Innis was right about the Cerebat takeover. And if the Wraw have marched that far, then the Semi Sisters are next.
+| (:annoyed)Now is no time for division - the Wraw are a threat to them as much as they are to us.
+| (:normal)I think you've come to earn \"Innis' trust\"(orange).
+| I want you to use whatever sway you have with her and \"ask her to stand with us\"(orange).
 ~ player
 - Got it.
-- I wouldn't count on it.
+- I wouldn't get your hopes up.
 - What if they join the Wraw?
-- [(var 'semis-weapons) They have weapons.]
+- [(var 'semis-weapons) They also have weapons.]
   ~ fi
-  | (:unsure)So they say, although I've never seen any.
-  | But I'll take what we can get.
+  | (:unsure)So they say, though I've never seen any.
+  | (:normal)But I'll take what we can get.
 ~ fi
 | Innis is a lot of things but she isn't stupid.
 | Just make sure you get to them first.
@@ -52,7 +52,8 @@
 ~ player
 - I will.
 - You too.
-- Be safe yourself.
+- I promise.
+! eval (setf (location 'trader) (location 'trader-semi-loc))
 ! eval (setf (walk 'fi) T)
 ! eval (move-to 'loc-trader (unit 'fi))
 ? (not (complete-p 'trader-arrive))
@@ -61,4 +62,4 @@
 ")
   (:eval
    :on-complete (q11-recruit-semis)))
-;; TODO hide Sahil at this stage, as if he's moved on (or move to a secret location, in hiding)
+;; moving Sahil back to his start location in the Semis base. Yes it's arguably closer to the frontline, but he wants strength in numbers, or to hide in a familiar corner maybe
