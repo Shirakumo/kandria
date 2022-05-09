cc12adb532661959e7f8bf41580b3cf5082aa03c
Tim White <timwhiteuk@gmail.com>
2022-05-09 13:06:26 +0100

    demo intro text tweak - make second line fit on one line, and make first line sound better than "middle of the story"

--- ui/demo-intro.lisp
+++ ui/demo-intro.lisp
@@ -6,9 +6,9 @@
 (defmethod initialize-instance :after ((panel demo-intro-panel) &key)
   (let ((layout (make-instance 'eating-constraint-layout
                                :shapes (list (simple:rectangle (unit 'ui-pass T) (alloy:margins) :pattern colors:black))))
-        (label (make-instance 'label :value "This demo showcases content from the middle of the story.
+        (label (make-instance 'label :value "This demo shows content from part way through the story.
 
-The complete game features a different intro, and many new environments, characters, and quests."
+The full game features a different intro, and many new environments, characters and quests."
                                      :style `((:label :halign :middle :pattern ,colors:black :size ,(alloy:un 15))))))
     (alloy:enter label layout :constraints '((:required (:max-width 1000) (:center :w :h))
                                        (:left 100) (:right 100) (:height 200)))
