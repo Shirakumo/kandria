b56220f296f4f731d17f3687963d449943b086b6
Tim White <timwhiteuk@gmail.com>
2022-06-15 17:43:35 +0100

    genera core item continuity

--- lang/eng/quests/trader-islay.lisp
+++ lang/eng/quests/trader-islay.lisp
@@ -97,7 +97,7 @@
 |   | (:nervous)Oh, that old chestnut...
 |   | (:normal)Before the Calamity there were hundreds of thousands of androids, and more in production.
 |   | Where did they all go? Were they all destroyed? - Present company excepted.
-|   | The truth is no one knows, and we'll probably never know.
+|   | Sometimes we find the occasional Genera core; but the truth is no one knows, and we'll probably never know.
 |   | But I doubt any are living in the mountains. We'd have seen or heard something, detected a signal... (:unhappy)But there's nothing.
 |   < talk
 | - I need to go.
