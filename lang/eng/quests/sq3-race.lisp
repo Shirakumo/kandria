a177725865b961ada265ad55fe89be337be11f94
Shinmera <shinmera@tymoon.eu>
2022-07-05 14:31:05 +0200

    contract this even more.

--- lang/eng/quests/sq3-race.lisp
+++ lang/eng/quests/sq3-race.lisp
@@ -1,24 +1,6 @@
 ;; -*- mode: poly-dialog; -*-
 (in-package #:org.shirakumo.fraf.kandria)
 
-(quest:define-quest (kandria sq3-race)
-  :author "Tim White"
-  :title "Catherine's Time Trials"
-  :description "Catherine and her friends want to see what I'm capable of. They've planted cans for me to find and bring back. The faster I can do it, the more parts I'll get."
-  :on-activate T
-  :variables (gold silver bronze pb start-task active-race)
-  (race-hub
-   :title "Talk to Catherine in Engineering to start a race"
-   :marker '(catherine 500)
-   :invariant (not (complete-p 'q10-wraw))
-   :condition all-complete
-   :on-activate T
-   :on-complete (sq3-race-1)
-   (:interaction about-races
-    :title "About the races."
-    :interactable catherine
-    :source '("catherine-race" "hub"))))
-
 (defmacro define-race (name &key site source marker title return-title description bronze silver gold on-complete)
   (let ((title-start (format NIL "(Start ~a)" title))
         (title-cancel (format NIL "(Cancel ~a)" title))
@@ -90,62 +72,69 @@
                   (reset* 'start 'race)
                   (clear-pending-interactions))))))
 
-;; TODO These vars stored in save game? Problematic if we tweak after launch/testing?
-(define-race sq3-race-1
-  :site race-1-site
-  :marker (chunk-1841 1400)
-  :title "Race Route 1"
-  :description "The can is at... a literal high point of EASTERN civilisation, now long gone."
-  :return-title "Return the can to Catherine in Engineering ASAP"
-  :source "catherine-race"
-  :gold 60
-  :silver 80
-  :bronze 120
-  :on-complete (sq3-race-2))
+(defmacro define-races (base-quest &body races)
+  (form-fiddle:with-body-options (races other source return) races
+    (declare (ignore other))
+    (let* ((quest (quest:find-named base-quest (quest:storyline 'kandria)))
+           (all-names (loop for i from 1 to (length races)
+                            collect (trial::mksym *package* base-quest '- i)))
+           (names all-names))
+      `(progn
+         ,@(loop for race in races
+                 for i from 1
+                 collect (destructuring-bind ((gold silver bronze) &rest body) race
+                           `(define-race ,(pop names)
+                              ,@body
+                              :gold ,gold
+                              :silver ,silver
+                              :bronze ,bronze
+                              :title ,(format NIL "~a ~d" (quest:title quest) i)
+                              :source ,source
+                              :on-complete (,(first names))
+                              :return-title ,return)))))))
 
-(define-race sq3-race-2
-  :site race-2-site
-  :marker (chunk-2480 3000)
-  :title "Race Route 2"
-  :description "The can is... where a shallow grave marks the end of the line at Zenith Crossing Station, East."
-  :return-title "Return the can to Catherine in Engineering ASAP"
-  :source "catherine-race"
-  :gold 60
-  :silver 80
-  :bronze 120
-  :on-complete (race-3))
+(quest:define-quest (kandria sq3-race)
+  :author "Tim White"
+  :title "Catherine's Time Trials"
+  :description "Catherine and her friends want to see what I'm capable of. They've planted cans for me to find and bring back. The faster I can do it, the more parts I'll get."
+  :on-activate T
+  :variables (gold silver bronze pb start-task active-race)
+  (race-hub
+   :title "Talk to Catherine in Engineering to start a race"
+   :marker '(catherine 500)
+   :invariant (not (complete-p 'q10-wraw))
+   :condition all-complete
+   :on-activate T
+   :on-complete (sq3-race-1)
+   (:interaction about-races
+    :title "About the races."
+    :interactable catherine
+    :source '("catherine-race" "hub"))))
 
-(define-race sq3-race-3
-  :site race-3-site
-  :marker (chunk-2482 1600)
-  :title "Race Route 3"
-  :description "The can is... beneath where we first ventured together, and got our feet wet."
-  :return-title "Return the can to Catherine in Engineering ASAP"
+(define-races sq3-race
   :source "catherine-race"
-  :gold 105
-  :silver 120
-  :bronze 150
-  :on-complete (sq3-race-4))
+  :return "Return the can to Catherine in Engineering ASAP"
+  ((60 80 120)
+   :site race-1-site
+   :marker (chunk-1841 1400)
+   :description "The can is at... a literal high point of EASTERN civilisation, now long gone.")
+  
+  ((60 80 120)
+   :site race-2-site
+   :marker (chunk-2480 3000)
+   :description "The can is... where a shallow grave marks the end of the line at Zenith Crossing Station, East.")
 
-(define-race sq3-race-4
-  :site race-4-site
-  :marker (chunk-5426 1600)
-  :title "Race Route 4"
-  :description "The can is... deep in the west, where we first met."
-  :return-title "Return the can to Catherine in Engineering ASAP"
-  :source "catherine-race"
-  :gold 90
-  :silver 105
-  :bronze 135
-  :on-complete (sq3-race-5))
+  ((105 120 150)
+   :site race-3-site
+   :marker (chunk-2482 1600)
+   :description "The can is... beneath where we first ventured together, and got our feet wet."    :bronze 150)
 
-(define-race sq3-race-5
-  :site race-5-site
-  :marker (chunk-2019 1600)
-  :title "Race Route 5"
-  :description "The can is at... the furthest edge of the deepest cave in this region - there isn't 'much-room'."
-  :return-title "Return the can to Catherine in Engineering ASAP"
-  :source "catherine-race"
-  :gold 135
-  :silver 150
-  :bronze 180)
+  ((90 105 135)
+   :site race-4-site
+   :marker (chunk-5426 1600)
+   :description "The can is... deep in the west, where we first met.")
+
+  ((135 150 180)
+   :site race-5-site
+   :marker (chunk-2019 1600)
+   :description "The can is at... the furthest edge of the deepest cave in this region - there isn't 'much-room'."))
