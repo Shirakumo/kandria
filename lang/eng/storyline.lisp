e3e9c962d3d7d2ae0def7a6ed09874c93f608c6e
Shinmera <shinmera@tymoon.eu>
2022-07-07 13:09:07 +0200

    Make things load

--- lang/eng/storyline.lisp
+++
@@ -1,6 +0,0 @@
-(in-package #:org.shirakumo.fraf.kandria)
-
-(quest:define-storyline kandria
-  :title "Kandria"
-  :description "The main kandria storyline"
-  :variables (q1-weld-burn take-walkie android-soft engineers-first-talk trader-daughter semis-weapons fight-army bomb-fee read-letter))
