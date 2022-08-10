736e4a40713928ce7bac932f6166d1f8544c38e8
Shinmera <shinmera@tymoon.eu>
2022-08-10 11:46:37 +0200

    scale mech default level to 20

--- enemy.lisp
+++ enemy.lisp
@@ -550,7 +550,7 @@
    (timer :initform 0.0 :accessor timer)
    (damage-input-scale :initform 0.5)
    (damage-accumulated :initform 0.0 :accessor damage-accumulated)
-   (level :initform 30))
+   (level :initform 20))
   (:default-initargs
    :sprite-data (asset 'kandria 'mech)))
 
