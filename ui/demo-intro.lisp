e03634576b9214d7fcea29eef2cd4f1c57c67956
Shinmera <shinmera@tymoon.eu>
2022-09-19 16:40:59 +0200

    not version in overlay

--- ui/demo-intro.lisp
+++ ui/demo-intro.lisp
@@ -16,7 +16,7 @@
 
 (defmethod initialize-instance :after ((panel prerelease-notice) &key)
   (let ((layout (make-instance 'org.shirakumo.alloy.layouts.constraint:layout))
-        (label (make-instance 'label :value "Pre-release version, do not record."
+        (label (make-instance 'label :value (format NIL "Pre-release version ~a, do not record." (version :app))
                                      :style `((:label :halign :middle :valign :middle :pattern ,(colored:color 1 1 1 0.5))))))
     (alloy:enter label layout :constraints `((:left 0) (:bottom 10) (:fill :w) (:height 20)))
     (alloy:finish-structure panel layout NIL)))
