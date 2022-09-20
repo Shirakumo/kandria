6522f50a2c4fb4c07823ad07f03fb1d64ca56d1f
Shinmera <shinmera@tymoon.eu>
2022-09-20 11:49:34 +0200

    Fix active effects

--- animatable.lisp
+++ animatable.lisp
@@ -350,4 +350,5 @@
 (defmethod render :after ((animatable animatable) (program shader-program))
   (when (active-effects animatable)
     (setf (location (active-effects-sprite animatable)) (location animatable))
+    (bind-textures (active-effects-sprite animatable))
     (render (active-effects-sprite animatable) program)))
