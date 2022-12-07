61d35eb51ae27656a102f1d6807fcda608b9adf6
Shinmera <shinmera@tymoon.eu>
2022-12-07 22:00:28 +0100

    Update release for new Trial release mechanism

--- releases/release.lisp
+++ releases/release.lisp
@@ -13,8 +13,20 @@
           :copy ("CHANGES.mess" "README.mess"
                                 ("bin/pool/trial/fps-texture.png"
                                  "pool/trial/fps-texture.png")))
+  :depots (:linux ("*.so" "kandria-linux.run")
+           :windows ("*.dll" "kandria-windows.exe")
+           :macos ("*.dylib" "kandria-macos.o")
+           :content ("pool/" "lang/" "world.zip" "CHANGES.mess" "README.mess" "keymap.lisp"))
+  :bundles (:linux (:depots (:linux :content))
+            :windows (:depots (:windows :content))
+            :macos (:depots (:macos :content))
+            :all (:depots (:linux :windows :macos :content)))
+  :keygen (:key "333B5B5C-9DDC-4E41-9C82-F2254330722E"
+           :secret "DCBE2959-5231-4D22-85BF-F12A9C4781D1"
+           :bundles (:linux 3
+                     :windows 2))
   :itch (:user "Shinmera")
   :steam (:branch "developer"
           :user "shirakumo_org")
-  :upload (:targets #(:steam))
+  :upload (:targets #(:steam :keygen))
   :system "kandria")
