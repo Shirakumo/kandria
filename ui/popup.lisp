bd40795289007003eb300105881dd355f63f3466
Shinmera <shinmera@tymoon.eu>
2023-02-18 11:42:09 +0100

    Try to be better with popups...

--- ui/popup.lisp
+++ ui/popup.lisp
@@ -14,6 +14,18 @@
     (alloy:decline ()
       T)))
 
+(defmethod alloy:refit ((layout popup-layout))
+  (let ((extent (alloy:bounds layout)))
+    (alloy:do-elements (element layout)
+      (let ((size (alloy:suggest-size (alloy:size 500 200) element)))
+        (when (or (= 0 (alloy:pxw size))
+                  (= 0 (alloy:pxh size)))
+          (setf size (alloy:bounds element)))
+        (setf (alloy:bounds element)
+              (alloy:px-extent (/ (- (alloy:pxw extent) (alloy:pxw size)) 2)
+                               (/ (- (alloy:pxh extent) (alloy:pxh size)) 2)
+                               (alloy:pxw size) (alloy:pxh size)))))))
+
 (defclass popup-button (button)
   ())
 
@@ -39,8 +51,8 @@
    :pattern colors:black
    :size (alloy:un 15)))
 
-(defclass popup-line (alloy:input-line alloy:direct-value-component)
-  ((alloy:value :initform "")))
+(defclass popup-line (alloy:input-line)
+  ())
 
 (presentations:define-realization (ui popup-line)
   ((background simple:rectangle)
@@ -54,14 +66,17 @@
    (alloy:margins 1)
    alloy:text
    :font (setting :display :font)
-   :size (alloy:un 20)
+   :size (alloy:un 14)
    :halign :start
    :valign :middle
    :pattern colors:black)
   ((:cursor simple:cursor)
    (presentations:find-shape :label alloy:renderable)
    0
-   :pattern colors:black))
+   :pattern colors:black)
+  ((:selection simple:selection)
+   (presentations:find-shape :label alloy:renderable)
+   0 0))
 
 (presentations:define-update (ui popup-line)
   (border
@@ -79,22 +94,20 @@
     (setf (slot-value panel 'alloy:layout-element) popup)))
 
 (defmethod show :after ((panel popup-panel) &key (width (alloy:un 300)) (height (alloy:un 120)))
-  (alloy:with-unit-parent (or (source panel) (unit 'ui-pass +world+))
-    (let ((bounds (if (source panel)
-                      (alloy:px-extent (alloy:pxx (alloy:global-location (source panel)))
+  (if (source panel)
+      (alloy:with-unit-parent (source panel)
+        (let ((bounds (alloy:px-extent (alloy:pxx (alloy:global-location (source panel)))
                                        (alloy:pxy (alloy:global-location (source panel)))
                                        (alloy:pxw (source panel))
-                                       (alloy:pxh (source panel)))
-                      (alloy:px-extent (- (alloy:to-px (alloy:vw 0.5)) (/ (alloy:to-px width) 2))
-                                       (- (alloy:to-px (alloy:vh 0.5)) (/ (alloy:to-px height) 2))
-                                       (alloy:to-px width) 0))))
-      (alloy:update (alloy:index-element 0 (alloy:layout-element panel))
-                    (alloy:layout-element panel)
-                    :x (+ (alloy:pxx bounds)
-                          (- (alloy:pxw bounds) (alloy:to-px width)))
-                    :y (alloy:pxy bounds)
-                    :w width
-                    :h height))))
+                                       (alloy:pxh (source panel)))))
+          (alloy:update (alloy:index-element 0 (alloy:layout-element panel))
+                        (alloy:layout-element panel)
+                        :x (+ (alloy:pxx bounds)
+                              (- (alloy:pxw bounds) (alloy:to-px width)))
+                        :y (alloy:pxy bounds)
+                        :w width
+                        :h height)))
+      (alloy:refit (alloy:layout-parent (alloy:layout-element panel)))))
 
 (defmethod hide :after ((panel popup-panel))
   (when (source panel)
@@ -170,7 +183,8 @@
                                                    :shapes (list (simple:rectangle (unit 'ui-pass T) (alloy:margins) :pattern colors:white))))
          (focus (make-instance 'alloy:focus-list))
          (label (make-instance 'info-label :value text :layout-parent layout))
-         (input (make-instance 'popup-line :layout-parent layout :focus-parent focus :placeholder placeholder))
+         (input (make-instance 'popup-line :layout-parent layout :focus-parent focus :placeholder placeholder
+                                           :data (make-instance 'alloy:value-data :value "")))
          (buttons (make-instance 'alloy:grid-layout :col-sizes '(T T) :row-sizes '(T) :layout-parent layout))
          (cancel (alloy:represent (@ dismiss-prompt-panel) 'popup-button :layout-parent buttons :focus-parent focus))
          (accept (alloy:represent (@ accept-prompt-panel) 'popup-button :layout-parent buttons :focus-parent focus)))
